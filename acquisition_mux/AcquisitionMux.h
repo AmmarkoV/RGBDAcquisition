853bad6f38a6cdd725dd387786d116aafe9059d6
Ammar Qammaz <ammarkov@gmail.com>
2013-01-29 16:38:17 +0200

    adding a muxed grabber to augment frames coming from different sources

---
+++ acquisition_mux/AcquisitionMux.h
@@ -0,0 +1,26 @@
+#ifndef ACQUISITIONMUX_H_INCLUDED
+#define ACQUISITIONMUX_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+struct AcquisitionMuxContext
+{
+   unsigned int NumberOfModules;
+
+
+};
+
+
+typedef unsigned int AcquisitionMultiplexerIdentifier;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // ACQUISITIONMUX_H_INCLUDED
