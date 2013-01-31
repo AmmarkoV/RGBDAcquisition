7c31044cfa13e1055e326fd214ea65fef28a34e9
Ammar Qammaz <ammarkov@gmail.com>
2013-01-31 12:22:32 +0200

    muxer almost ready

--- acquisition_mux/main.c
+++ acquisition_mux/main.c
@@ -1,23 +1,65 @@
 #include "AcquisitionMux.h"
 
 
-char * mux2RGBAndDepthFrames( char * rgb1, char * rgb2 , char * rgbOut , short * depth1, short * depth2 , short * depthOut , unsigned int width , unsigned int height , unsigned int mux_type)
+
+char * mux2RGBAndDepthFramesNonZeroDepth( char * rgb1, char * rgb2 , char * rgbOut , short * depth1, short * depth2 , short * depthOut , unsigned int width , unsigned int height , unsigned int mux_type)
 {
    char * rgb_p1 = rgb1;  char * rgb_p1_limit=rgb1 + width * height * 3;
    char * rgb_p2 = rgb2;  char * rgb_p2_limit=rgb2 + width * height * 3;
    char * rgb_pOut = rgbOut; char * rgb_pOut_limit=rgb_pOut + width * height * 3;
 
-   char * depth_p1 = depth1;  char * depth_p1_limit=rgb1 + width * height * 2;
-   char * depth_p2 = depth2;  char * depth_p2_limit=rgb2 + width * height * 2;
-   char * depth_pOut = depthOut; char * depth_pOut_limit=rgb_pOut + width * height * 2;
+   short * depth_p1 = depth1;     short * depth_p1_limit=rgb1 + width * height * 2;
+   short * depth_p2 = depth2;     short * depth_p2_limit=rgb2 + width * height * 2;
+   short * depth_pOut = depthOut; short * depth_pOut_limit=rgb_pOut + width * height * 2;
 
    while (rgb_p1<rgb_p1_limit)
     {
+        if (*depth_p2==0)
+         {
+           //IF DEPTH2 is not set we just ignore it an make a direct copy of the pixel data from RGB1/DEPTH1
+           *rgb_pOut = *rgb_p1;
+            rgb_pOut++; rgb_p1++;
+           *rgb_pOut = *rgb_p1;
+            rgb_pOut++; rgb_p1++;
+           *rgb_pOut = *rgb_p1;
+            rgb_pOut++; rgb_p1++;
 
-        //if ()
+            //RGB2 is just ignored
+            rgb_p2+=3;
 
+
+           *depth_pOut = *depth1;
+            depth_pOut++; depth1++;
+
+
+            //DEPTH2 is just ignored
+            ++depth2;
+         } else
+         {
+            //if depth 2 is not null we keep it as an overlay on RGB1 and depth1
+           *rgb_pOut = *rgb_p2;
+            rgb_pOut++; rgb_p2++;
+           *rgb_pOut = *rgb_p2;
+            rgb_pOut++; rgb_p2++;
+           *rgb_pOut = *rgb_p2;
+            rgb_pOut++; rgb_p2++;
+
+            //RGB1 is just ignored
+            rgb_p1+=3;
+
+
+           *depth_pOut = *depth1;
+           *depth_pOut += *depth2;
+            depth_pOut++; depth1++;  ++depth2;
+         }
 
     }
+}
+
+
+char * mux2RGBAndDepthFrames( char * rgb1, char * rgb2 , char * rgbOut , short * depth1, short * depth2 , short * depthOut , unsigned int width , unsigned int height , unsigned int mux_type)
+{
+
 
 
 
--- grabber_mux/main.c
+++ grabber_mux/main.c
@@ -26,17 +26,19 @@ int main(int argc, char *argv[])
  unsigned int possibleModules = acquisitionGetModulesCount();
  fprintf(stderr,"Linked to %u modules.. \n",possibleModules);
 
-
-  ModuleIdentifier moduleID = OPENGL_ACQUISITION_MODULE;//OPENNI1_ACQUISITION_MODULE;//
-
  if (possibleModules==0)
     {
        fprintf(stderr,"Acquisition Library is linked to zero modules , can't possibly do anything..\n");
        return 1;
     }
 
-    //We want to grab multiple frames in this example if the user doesnt supply a parameter default is 10..
-    unsigned int frameNum=0,maxFramesToGrab=10;
+
+ //We want to grab multiple frames in this example if the user doesnt supply a parameter default is 10..
+  unsigned int frameNum=0,maxFramesToGrab=10;
+  ModuleIdentifier moduleID_1 = OPENGL_ACQUISITION_MODULE;//OPENNI1_ACQUISITION_MODULE;//
+  ModuleIdentifier moduleID_2 = TEMPLATE_ACQUISITION_MODULE;//OPENNI1_ACQUISITION_MODULE;//
+
+  /*! --------------------------------- INITIALIZATION FROM COMMAND LINE PARAMETERS --------------------------------- */
     if (argc>1)
      {
           maxFramesToGrab=atoi(argv[1]);
@@ -44,25 +46,34 @@ int main(int argc, char *argv[])
      }
     if (argc>2)
      {
-          moduleID = getModuleIdFromModuleName(argv[2]);
-          fprintf(stderr,"Overriding Module Used , set to %s ( %u ) \n",getModuleStringName(moduleID),moduleID);
+          moduleID_1 = getModuleIdFromModuleName(argv[2]);
+          fprintf(stderr,"Overriding Module Used as device A , set to %s ( %u ) \n",getModuleStringName(moduleID_1),moduleID_1);
      }
-
-    strcpy(outputfoldername,"frames/");
     if (argc>3)
      {
-          strcat(outputfoldername,argv[3]);
+          moduleID_2 = getModuleIdFromModuleName(argv[3]);
+          fprintf(stderr,"Overriding Module Used as device B , set to %s ( %u ) \n",getModuleStringName(moduleID_2),moduleID_2);
+     }
+    strcpy(outputfoldername,"frames/");
+    if (argc>4)
+     {
+          strcat(outputfoldername,argv[4]);
           makepath(outputfoldername);
           fprintf(stderr,"OutputPath , set to %s  \n",outputfoldername);
      }
 
-  if (!acquisitionIsModuleLinked(moduleID))
+  if (!acquisitionIsModuleLinked(moduleID_1))
    {
-       fprintf(stderr,"The module you are trying to use is not linked in this build of the Acquisition library..\n");
+       fprintf(stderr,"The module you are trying to use as module A is not linked in this build of the Acquisition library..\n");
        return 1;
    }
 
-
+  if (!acquisitionIsModuleLinked(moduleID_2))
+   {
+       fprintf(stderr,"The module you are trying to use as module B is not linked in this build of the Acquisition library..\n");
+       return 1;
+   }
+  /*! --------------------------------- INITIALIZATION FROM COMMAND LINE PARAMETERS END --------------------------------- */
 
 
 
@@ -81,38 +92,38 @@ int main(int argc, char *argv[])
 
 
   //We need to initialize our module before calling any related calls to the specific module..
-  if (!acquisitionStartModule(moduleID,16 /*maxDevices*/ , 0 ))
+  if (!acquisitionStartModule(moduleID_1,16 /*maxDevices*/ , 0 ))
   {
-       fprintf(stderr,"Could not start module..\n");
+       fprintf(stderr,"Could not start module A ..\n");
+       return 1;
+   }
+  if (!acquisitionStartModule(moduleID_2,16 /*maxDevices*/ , 0 ))
+  {
+       fprintf(stderr,"Could not start module B ..\n");
        return 1;
    }
 
+
   //We want to initialize all possible devices in this example..
-  unsigned int devID=0,maxDevID=acquisitionGetModuleDevices(moduleID);
+  unsigned int devID_1=0 , devID_2=0;
 
 
-  if (maxDevID==0)
-  {
-      fprintf(stderr,"No devices found for Module used \n");
-      return 1;
-  }
 
     //Initialize Every OpenNI Device
-    for (devID=0; devID<maxDevID; devID++)
-     {
-        /*The first argument (Dev ID) could also be ANY_OPENNI2_DEVICE for a single camera setup */
-        acquisitionOpenDevice(moduleID,devID,640,480,25);
-        acquisitionMapDepthToRGB(moduleID,devID);
-        //acquisitionMapRGBToDepth(moduleID,devID);
-     }
+    acquisitionOpenDevice(moduleID_1,devID_1,640,480,25);
+    acquisitionMapDepthToRGB(moduleID_1,devID_1);
+
+    acquisitionOpenDevice(moduleID_2,devID_2,640,480,25);
+    acquisitionMapDepthToRGB(moduleID_2,devID_2);
+
+
+
     usleep(1000); // Waiting a while for the glitch frames to pass
     char outfilename[512]={0};
 
    for (frameNum=0; frameNum<maxFramesToGrab; frameNum++)
     {
-
-    for (devID=0; devID<maxDevID; devID++)
-      {
+        /*
         acquisitionSnapFrames(moduleID,devID);
 
         //fprintf(stderr,"Color frame is %ux%u:3 - %u \n",getOpenNI2ColorWidth(devID) , getOpenNI2ColorHeight(devID) , getOpenNI2ColorDataSize(devID));
@@ -122,21 +133,19 @@ int main(int argc, char *argv[])
         //fprintf(stderr,"Depth frame is %ux%u:1 - %u \n",getOpenNI2DepthWidth(devID) , getOpenNI2DepthHeight(devID) , getOpenNI2DepthDataSize(devID));
         sprintf(outfilename,"%s/depthFrame_%u_%05u.pnm",outputfoldername,devID,frameNum);
         acquisitionSaveDepthFrame(moduleID,devID,outfilename);
-      }
+*/
     }
 
+
+
+
     fprintf(stderr,"Done grabbing %u frames! \n",maxFramesToGrab);
-
-    for (devID=0; devID<maxDevID; devID++)
-     {
-        /*The first argument (Dev ID) could also be ANY_OPENNI2_DEVICE for a single camera setup */
-        acquisitionCloseDevice(moduleID,devID);
-     }
-
-    acquisitionStopModule(moduleID);
+    acquisitionCloseDevice(moduleID_1,devID_1);
+    acquisitionCloseDevice(moduleID_2,devID_2);
 
 
-
+    acquisitionStopModule(moduleID_1);
+    acquisitionStopModule(moduleID_2);
 
 
     return 0;
