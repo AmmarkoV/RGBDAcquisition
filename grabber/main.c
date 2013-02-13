a48310e02a177b51481d1d5c1e4a0ce141cb55c7
Ammar Qammaz <ammarkov@gmail.com>
2013-02-13 16:20:59 +0200

    ...

--- grabber/main.c
+++ grabber/main.c
@@ -110,11 +110,11 @@ int main(int argc, char *argv[])
         sprintf(outfilename,"%s/depthFrame_%u_%05u.pnm",outputfoldername,devID,frameNum);
         acquisitionSaveDepthFrame(moduleID,devID,outfilename);
 
-        sprintf(outfilename,"%s/depthFrame1C_%u_%05u.pnm",outputfoldername,devID,frameNum);
-        acquisitionSaveDepthFrame1C(moduleID,devID,outfilename);
+        //sprintf(outfilename,"%s/depthFrame1C_%u_%05u.pnm",outputfoldername,devID,frameNum);
+        //acquisitionSaveDepthFrame1C(moduleID,devID,outfilename);
 
-        sprintf(outfilename,"%s/coloreddepthFrame_%u_%05u.pnm",outputfoldername,devID,frameNum);
-        acquisitionSaveColoredDepthFrame(moduleID,devID,outfilename);
+        //sprintf(outfilename,"%s/coloreddepthFrame_%u_%05u.pnm",outputfoldername,devID,frameNum);
+        //acquisitionSaveColoredDepthFrame(moduleID,devID,outfilename);
       }
     }
 
