62b9dd87123d62510fda5f2f92c1f0532ffe1dac
Ammar Qammaz <ammarkov@gmail.com>
2013-05-28 18:50:50 +0300

    ...

---
+++ v4l2stereo_acquisition_shared_library/V4L2StereoAcquisition.c
@@ -0,0 +1,124 @@
+#include "V4L2StereoAcquisition.h"
+
+int startV4L2Stereo(unsigned int max_devs,char * settings)
+{
+ return 0;
+}
+
+int getV4L2Stereo()
+{
+ return 0;
+} // This has to be called AFTER startV4L2Stereo
+
+int stopV4L2Stereo()
+{
+ return 0;
+}
+
+int getDevIDForV4L2StereoName(char * devName)
+{
+ return 0;
+}
+
+   //Basic Per Device Operations
+int createV4L2StereoDevice(int devID,char * devName,unsigned int width,unsigned int height,unsigned int framerate)
+{
+ return 0;
+}
+
+int destroyV4L2StereoDevice(int devID)
+{
+ return 0;
+}
+
+int seekV4L2StereoFrame(int devID,unsigned int seekFrame)
+{
+ return 0;
+}
+
+int snapV4L2StereoFrames(int devID)
+{
+ return 0;
+}
+
+//Color Frame getters
+int getV4L2StereoColorWidth(int devID)
+{
+ return 0;
+}
+
+int getV4L2StereoColorHeight(int devID)
+{
+ return 0;
+}
+
+int getV4L2StereoColorDataSize(int devID)
+{
+ return 0;
+}
+
+int getV4L2StereoColorChannels(int devID)
+{
+ return 0;
+}
+
+int getV4L2StereoColorBitsPerPixel(int devID)
+{
+ return 0;
+}
+
+char * getV4L2StereoColorPixels(int devID)
+{
+ return 0;
+}
+
+double getV4L2StereoColorFocalLength(int devID)
+{
+ return 0;
+}
+
+double getV4L2StereoColorPixelSize(int devID)
+{
+ return 0;
+}
+
+   //Depth Frame getters
+int getV4L2StereoDepthWidth(int devID)
+{
+ return 0;
+}
+
+int getV4L2StereoDepthHeight(int devID)
+{
+ return 0;
+}
+
+int getV4L2StereoDepthDataSize(int devID)
+{
+ return 0;
+}
+
+int getV4L2StereoDepthChannels(int devID)
+{
+ return 0;
+}
+
+int getV4L2StereoDepthBitsPerPixel(int devID)
+{
+ return 0;
+}
+
+char * getV4L2StereoDepthPixels(int devID)
+{
+ return 0;
+}
+
+double getV4L2StereoDepthFocalLength(int devID)
+{
+ return 0;
+}
+
+double getV4L2StereoDepthPixelSize(int devID)
+{
+ return 0;
+}
---
+++ v4l2stereo_acquisition_shared_library/V4L2StereoAcquisition.h
@@ -0,0 +1,48 @@
+#ifndef V4L2STEREOACQUISITION_H_INCLUDED
+#define V4L2STEREOACQUISITION_H_INCLUDED
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+   //Initialization of V4L2
+   int startV4L2StereoStereo(unsigned int max_devs,char * settings);
+   int getV4L2Stereo(); // This has to be called AFTER startV4L2Stereo
+   int stopV4L2Stereo();
+
+   int getDevIDForV4L2StereoName(char * devName);
+
+   //Basic Per Device Operations
+   int createV4L2StereoDevice(int devID,char * devName,unsigned int width,unsigned int height,unsigned int framerate);
+   int destroyV4L2StereoDevice(int devID);
+
+   int seekV4L2StereoFrame(int devID,unsigned int seekFrame);
+   int snapV4L2StereoFrames(int devID);
+
+   //Color Frame getters
+   int getV4L2StereoColorWidth(int devID);
+   int getV4L2StereoColorHeight(int devID);
+   int getV4L2StereoColorDataSize(int devID);
+   int getV4L2StereoColorChannels(int devID);
+   int getV4L2StereoColorBitsPerPixel(int devID);
+   char * getV4L2StereoColorPixels(int devID);
+   double getV4L2StereoColorFocalLength(int devID);
+   double getV4L2StereoColorPixelSize(int devID);
+
+   //Depth Frame getters
+   int getV4L2StereoDepthWidth(int devID);
+   int getV4L2StereoDepthHeight(int devID);
+   int getV4L2StereoDepthDataSize(int devID);
+   int getV4L2StereoDepthChannels(int devID);
+   int getV4L2StereoDepthBitsPerPixel(int devID);
+
+   char * getV4L2StereoDepthPixels(int devID);
+   double getV4L2StereoDepthFocalLength(int devID);
+   double getV4L2StereoDepthPixelSize(int devID);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // V4L2STEREOACQUISITION_H_INCLUDED
