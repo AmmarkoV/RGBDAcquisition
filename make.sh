e7bc301a731dbe8a90a11eaf9bc1a09b2f077ced
Ammar Qammaz <ammarkov@gmail.com>
2013-05-31 17:32:02 +0300

    ...

--- make.sh
+++ make.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if [ -d openni1_acquisition_shared_library ]
+then  
+cd openni1_acquisition_shared_library
+ln -s OpenNI1Acquisition.so libOpenNI1Acquisition.so
+cd ..
+fi
+
+if [ -d openni2_acquisition_shared_library ]
+then  
+cd openni2_acquisition_shared_library
+ln -s OpenNI2Acquisition.so libOpenNI2Acquisition.so
+cd ..
+fi
+
 if [ -d grabber ]
 then  
 cd grabber
