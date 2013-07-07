495eab3ea443400be8227d677331d90d37ff93a1
Ammar Qammaz <ammarkov@gmail.com>
2013-07-07 23:35:20 +0300

    adding makefile for tools

--- scripts/update_makefile.sh
+++ scripts/update_makefile.sh
@@ -3,4 +3,8 @@
 #uses cbp2make , get it here - > http://sourceforge.net/projects/cbp2make/
 cbp2make -in CodeBlocks.workspace -unix -out makefile
 
+cd tools
+cbp2make -in Tools.workspace -unix -out makefile
+cd ..
+
 exit 0
