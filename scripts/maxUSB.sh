e204a6f8919344a3ab5e82d738aaaacee0fae96a
Ammar Qammaz <ammarkov@gmail.com>
2013-12-17 13:11:17 +0200

    adding an image for the project

--- scripts/maxUSB.sh
+++ scripts/maxUSB.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+#Set maximum USB Performance , and allocate a LOT of USB space
 sudo modprobe usbcore usbfs_memory_mb=1000
+
+#Run what we wanted to run at top priority 
 sudo nice -n -20 ionice -c 1 -n 0 $@
 
-exit 0
+exit $?
