6b50caa2d6ef8f3014e3e7bcfc5aa1463c3f5254
Ammar Qammaz <ammarkov@gmail.com>
2014-01-24 21:09:44 +0200

    doxygen files can now be built on any pc ( not just mine ;P )

--- scripts/refreshDoxygen.sh
+++ scripts/refreshDoxygen.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+#Switch to this directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+ 
+export RGBD_DOXYGEN_INPUT="$DIR/.."
+export RGBD_DOXYGEN_OUTPUT="$DIR/../doc"
+
+cd ..
 doxygen doc/doxyfile
 cd doc/latex
 make
@@ -6,5 +15,9 @@ cd ..
 cd ..
 
 ln -s doc/latex/refman.pdf RGBDAcquisition.pdf
+
+
+cd "$STARTDIR"
+ 
 
 exit 0
