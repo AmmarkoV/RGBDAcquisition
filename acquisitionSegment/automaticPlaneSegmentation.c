121bf1849a82bb6d010b7d5e753e23177526177d
Ammar Qammaz <ammarkov@gmail.com>
2014-03-27 10:58:29 +0200

    ...

--- acquisitionSegment/automaticPlaneSegmentation.c
+++ acquisitionSegment/automaticPlaneSegmentation.c
@@ -2,7 +2,7 @@
 
 
 
-int automaticPlaneSegmentation()
+int automaticPlaneSegmentation(unsigned short * source , unsigned int width , unsigned int height , struct SegmentationFeaturesDepth * segConf )
 {
 
 
