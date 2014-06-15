e576caf7cfb28fdf0b1a83da65a867d3ffc0a463
Ammar Qammaz <ammarkov@gmail.com>
2014-06-15 05:00:10 +0300

    ...

--- tools/ViewpointChange/ViewpointChange.h
+++ tools/ViewpointChange/ViewpointChange.h
@@ -9,12 +9,15 @@ extern "C"
 #endif
 
 
+unsigned char * viewPointChange_ReadPPM(char * filename,unsigned int * width , unsigned int * height , unsigned int * channels,unsigned int * bitsperpixel,char read_only_header);
 
 unsigned int viewPointChange_fitImageInMask(unsigned char * img, unsigned char * mask , unsigned int width , unsigned int height);
 unsigned char* viewPointChange_mallocTransformToBirdEyeView(unsigned char *  rgb , unsigned short *  depth, unsigned int width , unsigned int height , unsigned int depthRange);
 int viewPointChange_newFramesCompare(unsigned char *  prototype , unsigned char *  rgb , unsigned short *  depth, unsigned int width , unsigned int height  , unsigned int depthRange );
 
-
+unsigned int viewPointChange_countDepths(unsigned short *  depth, unsigned int imageWidth , unsigned int imageHeight  ,
+                                unsigned int x , unsigned int y , unsigned int width , unsigned int height ,
+                                unsigned int depthRange );
 
 
 #ifdef __cplusplus
