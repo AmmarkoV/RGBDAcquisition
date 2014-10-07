ca76cbd0fb0167e615f75847d24f2f7d07fd5b0d
Ammar Qammaz <ammarkov@gmail.com>
2014-10-07 12:57:46 +0300

    ...

--- 3dparty/link_to_libs.sh
+++ 3dparty/link_to_libs.sh
@@ -9,6 +9,21 @@ else
 fi
 
 
+ONIFOLDER64="x64-Release"
+ONIFOLDER="x86-Release"
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+echo "Will pick 64bit binaries"
+ONIFOLDER=$ONIFOLDER64
+else
+echo "Will pick 32bit binaries"
+#CUDA_VER SHOULD ALREADY BE SET TO $CUDA_VER32
+fi
+
+
+
+
+
 if [ -d "$DIR_WITH_3D_PARTY_PLUGINS/libfreenect" ]
 then
 echo "Linking to freenect libs"  
@@ -23,11 +38,11 @@ fi
 if [ -d "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI" ]
 then
 echo "Linking to OpenNI1 libs"
-ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI/Platform/Linux/Bin/x64-Release/libOpenNI.so"
-ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI/Platform/Linux/Bin/x64-Release/libOpenNI.jni.so" 
-ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI/Platform/Linux/Bin/x64-Release/libnimCodecs.so"
-ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI/Platform/Linux/Bin/x64-Release/libnimMockNodes.so"
-ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI/Platform/Linux/Bin/x64-Release/libnimRecorder.so"
+ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI/Platform/Linux/Bin/$ONIFOLDER/libOpenNI.so"
+ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI/Platform/Linux/Bin/$ONIFOLDER/libOpenNI.jni.so" 
+ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI/Platform/Linux/Bin/$ONIFOLDER/libnimCodecs.so"
+ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI/Platform/Linux/Bin/$ONIFOLDER/libnimMockNodes.so"
+ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI/Platform/Linux/Bin/$ONIFOLDER/libnimRecorder.so"
 else
  echo "Could not find OpenNI1 directory"
 fi
@@ -36,10 +51,10 @@ fi
 if [ -d "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI2" ]
 then
 echo "Linking to OpenNI2 libs"
-ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI2/Bin/x64-Release/OpenNI2/"
+ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI2/Bin/$ONIFOLDER/OpenNI2/"
 ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI2/Config/OpenNI.ini"
 ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI2/Config/PS1080.ini"
-ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI2/Bin/x64-Release/libOpenNI2.so"
+ln -s "$DIR_WITH_3D_PARTY_PLUGINS/OpenNI2/Bin/$ONIFOLDER/libOpenNI2.so"
 else
  echo "Could not find OpenNI2 directory"
 fi
