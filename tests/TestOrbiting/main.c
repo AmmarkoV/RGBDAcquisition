45f2e9c5aa7b85b683087038083ad3658e201ea8
Ammar Qammaz <ammarkov@gmail.com>
2014-12-17 19:18:21 +0200

    ...

--- tests/TestOrbiting/main.c
+++ tests/TestOrbiting/main.c
@@ -4,39 +4,6 @@
 
 
 
-int affixSatteliteToPlanetFromFrameForLength(
-                                              double * satPosAbsolute ,
-                                              double * planetPosAbsolute ,
-                                              double * planetRotAbsolute,
-                                              double * satPosRelative
-                                             )
-{
-    //There is literally no good reason to go from rotation -> quaternion -> 3x3 -> quaternion -> rotation this could be optimized
-    //==================================================================================
-
- /*
-    unsigned int pos=0;
-    for (pos=frameNumber+1; pos<frameNumber+duration; pos++)
-    {
-       planetPosAbsolute[0] = (double) stream->object[planetObj].frame[pos].x;
-       planetPosAbsolute[1] = (double) stream->object[planetObj].frame[pos].y;
-       planetPosAbsolute[2] = (double) stream->object[planetObj].frame[pos].z;
-       planetPosAbsolute[3] = 1.0;
-
-       planetRotAbsolute[0] = (double) stream->object[planetObj].frame[pos].rot1;
-       planetRotAbsolute[1] = (double) stream->object[planetObj].frame[pos].rot2;
-       planetRotAbsolute[2] = (double) stream->object[planetObj].frame[pos].rot3;
-
-       if ( pointFromRelationWithObjectToAbsolute_PosXYZRotationXYZ(satPosAbsolute,planetPosAbsolute,planetRotAbsolute,satPosRelative) )
-       {
-           stream->object[satteliteObj].frame[pos].x = (float) satPosAbsolute[0];
-           stream->object[satteliteObj].frame[pos].y = (float) satPosAbsolute[1];
-           stream->object[satteliteObj].frame[pos].z = (float) satPosAbsolute[2];
-       }
-    }*/
- return 1;
-
-}
 
 
 int generatePlot(unsigned int viewX,unsigned int viewY)
