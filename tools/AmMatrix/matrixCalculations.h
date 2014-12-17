467c8ad345a2f7ca2a8b704f46ecf11853ea8dfb
Ammar Qammaz <ammarkov@gmail.com>
2014-12-17 17:38:23 +0200

    clearing up headers and adding comments where they do not exist

--- tools/AmMatrix/matrixCalculations.h
+++ tools/AmMatrix/matrixCalculations.h
@@ -7,19 +7,51 @@
 #define MATRIXCALCULATIONS_H_INCLUDED
 
 
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-
-
+/**
+* @brief build Projection Matrix using Rodriguez Rotation and a translation ( typically coming from OpenCV )
+* @ingroup AmMatrix
+* @param  Output Array 4x4 of resulting matrix
+* @param  Input Rodriguez Coordinates
+* @param  Input Translation Coordinate
+* @param  Input Unit Scale
+* @retval 0=Failure,1=Success
+*/
 int convertRodriguezAndTranslationTo4x4DUnprojectionMatrix(double * result4x4, double * rodriguez , double * translation , double scaleToDepthUnit);
+
+/**
+* @brief build OpenGL Projection Matrix using Rodriguez Rotation and a translation ( typically coming from OpenCV )
+* @ingroup AmMatrix
+* @param  Output Array 4x4 of resulting matrix
+* @param  Input Rodriguez Coordinates
+* @param  Input Translation Coordinate
+* @param  Input Unit Scale
+* @retval 0=Failure,1=Success
+*/
 int convertRodriguezAndTranslationToOpenGL4x4DProjectionMatrix(double * result4x4, double * rodriguez , double * translation , double scaleToDepthUnit);
 
-int convertTranslationTo4x4(double * translation, double * result);
 
+/**
+* @brief Project a 3D Point to a 2D surface , emulating a camera
+* @ingroup AmMatrix
+* @param  Output 2D X Point
+* @param  Output 2D Y Point
+
+* @param  Input 3D X Point
+* @param  Input 3D Y Point
+* @param  Input 3D Z Point
+
+
+* @param  Input Intrinsics Matrix
+
+* @param  Input Rotation 3x3 Matrix
+* @param  Input Translation 3x1
+* @retval 0=Failure,1=Success
+*/
 int projectPointsFrom3Dto2D(double * x2D, double * y2D , double * x3D, double *y3D , double * z3D , double * intrinsics , double * rotation3x3 , double * translation);
 
 
@@ -55,13 +87,18 @@ void buildOpenGLProjectionForIntrinsics   (
 * @param  Input Array 4x1 of absolute 3D position of the point ( X,Y,Z,W )
 * @retval 0=Failure,1=Success
 */
-int pointInRelationToObject(double * relativeOutPoint3DUnrotated, double * objectPosition , double * objectRotation3x3 , double * absoluteInPoint3DRotated );
-
-
 int pointFromRelationToObjectXYZQuaternionXYZWToAbsolute(unsigned int method,  double * absoluteInPoint3DRotated , double * objectPosition , double * objectQuaternion ,double * relativeOutPoint3DUnrotated);
 
 
 
+/**
+* @brief Multiply a 4x4 matrix with a Vector (3D Point)  A*V
+* @ingroup AmMatrix
+* @param  Output Vector ( should be already allocated )
+* @param  Input 4x4 Matrix A
+* @param  Input Vector 4x1 V
+* @retval 0=failure,1=success
+*/
 int move3DPoint(double * resultPoint3D, double * transformation4x4, double * point3D  );
 
 
@@ -69,9 +106,7 @@ int move3DPoint(double * resultPoint3D, double * transformation4x4, double * poi
 
 
 int pointFromRelationWithObjectToAbsolute(double * absoluteOutPoint3DRotated, double * objectPosition , double * objectRotation3x3 ,  double * relativeInPoint3DUnrotated);
-
 int pointFromAbsoluteToInRelationWithObject(double * relativeOutPoint3DUnrotated, double * objectPosition , double * objectRotation3x3 , double * absoluteInPoint3DRotated );
-int pointFromAbsoluteToInRelationWithObject_UsingInversion(double * relativeOutPoint3DUnrotated, double * objectPosition , double * objectRotation3x3 , double * absoluteInPoint3DRotated );
 
 int pointFromAbsoluteToRelationWithObject_PosXYZRotationXYZ(double * relativeOutPoint3DUnrotated, double * objectPosition , double * objectRotation , double * absoluteInPoint3DRotated );
 int pointFromAbsoluteToRelationWithObject_PosXYZQuaternionXYZW(double * relativeOutPoint3DUnrotated, double * objectPosition , double * objectQuaternion , double * absoluteInPoint3DRotated );
@@ -147,9 +182,35 @@ Found at : http://ilab.usc.edu/wiki/index.php/Fast_Square_Root
 inline float sqrt_fast_approximation(const float x);
 
 
+/**
+* @brief Count the distance between 2 points
+* @ingroup AmMatrix
+* @param  Point A
+* @param  Point B
+* @retval Distance between the two points
+*/
 double distanceBetween3DPoints(double * p1, double * p2);
+
+/**
+* @brief Find an approximation of the distance between 2 points
+* @ingroup AmMatrix
+* @param  Point A
+* @param  Point B
+* @retval Distance between the two points
+* @bug Using an approximation of the distance between 2 points is fast , but it should only be used for approximations and not accurate results
+*/
 float distanceBetween3DPointsFast(float *x1,float*y1,float *z1,float *x2,float*y2,float *z2);
+
+
+/**
+* @brief Count the squared distance between 2 points
+* @ingroup AmMatrix
+* @param  Point A
+* @param  Point B
+* @retval Squared Distance between the two points
+*/
 float squaredDistanceBetween3DPoints(float *x1,float*y1,float *z1,float *x2,float*y2,float *z2);
+
 
 /**
 * @brief Calculate the distance between two 3d points
