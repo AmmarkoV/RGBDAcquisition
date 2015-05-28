f508a9c018eca88730b9a0a412c7b8807b9e975c
Ammar Qammaz <ammarkov@gmail.com>
2015-05-28 13:31:41 +0300

    ...

--- grabber/frames/dobble/createDataset.sh
+++ grabber/frames/dobble/createDataset.sh
@@ -14,12 +14,18 @@ do
   
 
 degs=0
-for (( i=0; i<=35; i++ ))
+for (( contrast=10; contrast<=11; contrast++ ))
 do   
-  convert classes/$f -resize $DIMENSION -background white -gravity center -extent $DIMENSION -distort SRT  -$degs  -quality 99  dataset/$TARGETNAME/$i.jpg
-  degs=$((degs+10))  
-  echo -n "."
-done 
+
+
+ for (( i=0; i<=360; i++ ))
+   do   
+    convert classes/$f +sigmoidal-contrast $contrast -resize $DIMENSION -background white -gravity center -extent $DIMENSION -distort SRT  -$degs  -quality 99  dataset/$TARGETNAME/$i-$contrast.jpg
+    degs=$((degs+1))  
+    echo -n "."
+   done 
+
+done
  
 done
 
