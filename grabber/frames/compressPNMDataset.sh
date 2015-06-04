e3519f73cbca4fa58f51b90c5d79784d9fac8243
Ammar Qammaz <ammarkov@ics.forth.gr>
2015-06-04 15:50:19 +0300

    ...

--- grabber/frames/compressPNMDataset.sh
+++ grabber/frames/compressPNMDataset.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
   
+ 
 
 INITIALSIZE=`du -hs $1 | cut -f1`
 
@@ -7,6 +8,23 @@ CONVNAME="tmp_under_conversion"
 cd $1
 
 
+
+COLORFILENUM=`ls -al | grep color | grep .pnm |  wc -l`
+DEPTHFILENUM=`ls -al | grep depth | grep .pnm |  wc -l`
+
+
+if [ $COLORFILENUM -eq 0 ]; then
+ echo "No color pnm files found , $1 is not a valid PNM dataset for compression..!"
+ exit 0
+fi
+if [ $DEPTHFILENUM -eq 0 ]; then
+ echo "No depth pnm files found , $1 is not a valid PNM dataset for compression..!"
+ exit 0
+fi
+
+echo "Looks $1 is an uncompressed dataset ($COLORFILENUM pnm color files , $DEPTHFILENUM pnm depth files ) will try to compress it now "
+
+exit 0
 mkdir ../$CONVNAME
 
 cp color.calib ../$CONVNAME/color.calib
@@ -33,13 +51,31 @@ done
 
 cd ..
 
-
-rm -rf $1
-mv $CONVNAME $1
-
-FINALSIZE=`du -hs $1| cut -f1`
-
+FINALSIZE=`du -hs $CONVNAME| cut -f1`
+  echo
+  echo   
 echo "Compressed from $INITIALSIZE to $FINALSIZE"
+
+
+echo "Overwrite old dataset ?"
+  echo
+  echo   
+  echo ""
+  echo
+  echo -n "            (Y/N)?"
+  read answer
+  if test "$answer" != "Y" -a "$answer" != "y";
+  then 
+   echo "Naming the compressed dataset $1-Compressed.." 
+   mv $CONVNAME "$1-Compressed" 
+else
+   echo "Overwriting.." 
+   rm -rf $1
+   mv $CONVNAME $1 
+fi
+
+ 
+
 
 
 
