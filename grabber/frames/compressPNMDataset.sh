f35f9bc55d0e82ee8b54e032ccb4a39c28c7a44b
Ammar Qammaz <ammarkov@ics.forth.gr>
2015-06-04 15:27:01 +0300

    small script to compress huge raw datasets

---
+++ grabber/frames/compressPNMDataset.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+  
+
+INITIALSIZE=`du -hs $1 | cut -f1`
+
+CONVNAME="tmp_under_conversion"
+cd $1
+
+
+mkdir ../$CONVNAME
+
+cp color.calib ../$CONVNAME/color.calib
+cp depth.calib ../$CONVNAME/depth.calib
+
+echo "Converting Color Files"
+
+FILES_TO_CONVERT=`ls | grep color`
+for f in $FILES_TO_CONVERT
+do 
+ TARGETNAME=`basename $f .pnm`
+ convert $f ../$CONVNAME/$TARGETNAME.png
+done
+
+
+echo "Converting Depth Files"
+
+FILES_TO_CONVERT=`ls | grep depth`
+for f in $FILES_TO_CONVERT
+do 
+ TARGETNAME=`basename $f .pnm`
+ ../../../tools/DepthImagesConverter/DepthImagesConverter "$f" "../$CONVNAME/$TARGETNAME.png"
+done
+
+cd ..
+
+
+rm -rf $1
+mv $CONVNAME $1
+
+FINALSIZE=`du -hs $1| cut -f1`
+
+echo "Compressed from $INITIALSIZE to $FINALSIZE"
+
+
+
+exit 0
