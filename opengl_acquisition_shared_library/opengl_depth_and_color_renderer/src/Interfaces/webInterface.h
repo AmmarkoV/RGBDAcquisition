a07c4833a65d91311fe0ffe8c669f5399aa73c22
Ammar Qammaz <ammarkov@gmail.com>
2015-07-10 12:40:30 +0300

    adding a web interface binding to ammarserver for remote control of the OGL sandbox ( this might not be the greatest idea ever..

---
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Interfaces/webInterface.h
@@ -0,0 +1,14 @@
+/** @file webInterface.h
+ *  @brief  A module that loads models from files so that they can be rendered
+ *  @author Ammar Qammaz (AmmarkoV)
+ */
+
+
+#ifndef WEB_INTERFACE_H_INCLUDED
+#define WEB_INTERFACE_H_INCLUDED
+
+
+
+#endif
+
+
