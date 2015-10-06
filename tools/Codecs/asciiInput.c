2ccd717b92a1baf571da93eabec2a96cc68e8701
Ammar Qammaz <ammarkov@gmail.com>
2015-10-06 17:05:39 +0300

    ascii input

--- tools/Codecs/asciiInput.c
+++ tools/Codecs/asciiInput.c
@@ -9,17 +9,20 @@
 #define PPMREADBUFLEN 256
 
 
-unsigned char * ReadASCII(unsigned char * buffer , char * filename,unsigned int *width,unsigned int *height,unsigned long * timestamp)
+unsigned char * ReadASCIIRaw(unsigned char * buffer , char * filename,unsigned int *width,unsigned int *height,unsigned long * timestamp)
 {
     //See http://en.wikipedia.org/wiki/Portable_anymap#File_format_description for this simple and useful format
     unsigned char * pixels=buffer;
-    if (pixels==0) {  pixels= (unsigned char*) malloc(w*h*bytesPerPixel*channels*sizeof(char)); }
     FILE *pf=0;
-    pf = fopen(filename,"rb");
+    pf = fopen(filename,"r");
 
     if (pf!=0 )
     {
         *width=0; *height=0; *timestamp=0;
+
+        //Todo read once
+         //if (pixels==0) {  pixels= (unsigned char*) malloc(w*h*bytesPerPixel*channels*sizeof(char)); }
+
 
         fclose(pf);
     } else
@@ -32,46 +35,61 @@ unsigned char * ReadASCII(unsigned char * buffer , char * filename,unsigned int 
 
 int ReadASCII(char * filename,struct Image * pic,char read_only_header)
 {
-  pic->pixels = ReadASCII(pic->pixels , filename, &pic->width, &pic->height, &pic->timestamp );
+  pic->pixels = ReadASCIIRaw(pic->pixels , filename, &pic->width, &pic->height, &pic->timestamp );
   return (pic->pixels!=0);
 }
 
 
 
-int WriteASCII(char * filename,struct Image * pic)
+int WriteASCII(char * filename,struct Image * pic,int packed)
 {
     //fprintf(stderr,"saveRawImageToFile(%s) called\n",filename);
     if (pic==0) { return 0; }
     if ( (pic->width==0) || (pic->height==0) || (pic->channels==0) || (pic->bitsperpixel==0) )
         {
-          fprintf(stderr,"saveRawImageToFile(%s) called with zero dimensions ( %ux%u %u channels %u bpp\n",filename,pic->width , pic->height,pic->channels,pic->bitsperpixel);
+          fprintf(stderr,"WriteASCII(%s) called with zero dimensions ( %ux%u %u channels %u bpp\n",filename,pic->width , pic->height,pic->channels,pic->bitsperpixel);
           return 0;
         }
-    if(pic->pixels==0) { fprintf(stderr,"saveRawImageToFile(%s) called for an unallocated (empty) frame , will not write any file output\n",filename); return 0; }
-    if (pic->bitsperpixel>16) { fprintf(stderr,"PNM does not support more than 2 bytes per pixel..!\n"); return 0; }
+    if(pic->pixels==0) { fprintf(stderr,"WriteASCII(%s) called for an unallocated (empty) frame , will not write any file output\n",filename); return 0; }
 
     FILE *fd=0;
-    fd = fopen(filename,"wb");
+    fd = fopen(filename,"w");
 
     if (fd!=0)
     {
-        unsigned int n;
-        if (pic->channels==3) fprintf(fd, "P6\n");
-        else if (pic->channels==1) fprintf(fd, "P5\n");
-        else
+
+      char * ptr = pic->pixels ;
+      unsigned int x , y;
+
+        if (packed)
         {
-            fprintf(stderr,"Invalid channels arg (%u) for SaveRawImageToFile\n",pic->channels);
-            fclose(fd);
-            return 1;
+           for (y=0; y<pic->height; y++)
+           {
+            for (x=0; x<pic->width; x++)
+             {
+               fprintf(fd, "%u ",*ptr);
+               ++ptr;
+             }
+             fprintf(fd, "%\n ");
+           }
+        } else
+        {
+         unsigned int channel=0;
+         for (channel=0; channel<3; channel++)
+         {
+           ptr = pic->pixels + channel;
+           for (y=0; y<pic->height; y++)
+           {
+            for (x=0; x<pic->width; x++)
+             {
+               fprintf(fd, "%u ",*ptr);
+               ptr+=3;
+             }
+             fprintf(fd, "%\n ");
+           }
+         }
         }
 
-        fprintf(fd, "%d %d\n%u\n", pic->width, pic->height , simplePow(2 ,pic->bitsperpixel)-1);
-
-        float tmp_n = (float) pic->bitsperpixel/ 8;
-        tmp_n = tmp_n *  pic->width * pic->height * pic->channels ;
-        n = (unsigned int) tmp_n;
-
-        fwrite(pic->pixels, 1 , n , fd);
         fflush(fd);
         fclose(fd);
         return 1;
--- tools/Codecs/asciiInput.h
+++ tools/Codecs/asciiInput.h
@@ -1,10 +1,11 @@
-#ifndef _PPMINPUT_H_INCLUDED
-#define _PPMINPUT_H_INCLUDED
+#ifndef _ASCIIINPUT_H_INCLUDED
+#define _ASCIIINPUT_H_INCLUDED
 
 #include "codecs.h"
 
-int ReadPPM(char * filename,struct Image * pic,char read_only_header);
-int WritePPM(char * filename,struct Image * pic);
+
+int ReadASCII(char * filename,struct Image * pic,char read_only_header);
+int WriteASCII(char * filename,struct Image * pic,int packed);
 
 
 #endif
