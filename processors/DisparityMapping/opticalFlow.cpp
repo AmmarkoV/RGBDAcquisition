549956ed2f2b4b781731c8eb0323412e8af4fd10
Ammar Qammaz <ammarkov@gmail.com>
2015-12-02 16:57:33 +0200

    ...

---
+++ processors/DisparityMapping/opticalFlow.cpp
@@ -0,0 +1,38 @@
+#include "opticalFlow.h"
+
+
+#include "cv.h"
+#include "cxmisc.h"
+#include "highgui.h"
+#include "cvaux.h"
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+int test()
+{
+    // winsize has to be 11 or 13, otherwise nothing is found
+    int winsize = 11;
+    int maxlvl = 5;
+
+    calcOpticalFlowPyrLK(imgAgray, imgBgray, cornersA, cornersB, status, error,
+            Size(winsize, winsize), maxlvl);
+
+    for (unsigned int i = 0; i < cornersB.size(); i++) {
+        if (status[i] == 0 || error[i] > 0) {
+            drawPixel(cornersB[i], &imgC, 2, red);
+            continue;
+        }
+        drawPixel(cornersB[i], &imgC, 2, green);
+        line(imgC, cornersA[i], cornersB[i], Scalar(255, 0, 0));
+    }
+
+    namedWindow("window", 1);
+    moveWindow("window", 50, 50);
+
+
+}
--- testProcessorDisparityMapping.sh
+++ testProcessorDisparityMapping.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
  
-#  firstGPSCar
-#./run_viewer.sh -module TEMPLATE -from allimiagpscalibra -seek 60  -noDepth -processor ../processors/DisparityMapping/libDisparityMapping.so  DisparityMapping -resizeWindow 1920 500 -maxFrames 200 -disparityCalibration /home/ammar/Documents/Programming/FORTH/input_acquisition/grabbed_frames/allimiagpscalibra/DisparityMappingCalibration  -disparitySADWindowSize 70 -disparitySwapColorFeeds $@
+#  firstGPSCar firstGPSCar2 allimiagpscalibra
+./run_viewer.sh -module TEMPLATE -from firstGPSCar2 -seek 60  -noDepth -processor ../processors/DisparityMapping/libDisparityMapping.so  DisparityMapping -resizeWindow 1920 500 -maxFrames 200 -disparityCalibration /home/ammar/Documents/Programming/FORTH/input_acquisition/grabbed_frames/allimiagpscalibra/DisparityMappingCalibration  -disparitySADWindowSize 70 -disparitySwapColorFeeds $@
 
 
 
-./run_viewer.sh -module TEMPLATE -from oldStereoSet -seek 10  -noDepth -processor ../processors/DisparityMapping/libDisparityMapping.so  DisparityMapping -maxFrames 200  -disparitySADWindowSize 25 $@
+#./run_viewer.sh -module TEMPLATE -from oldStereoSet -seek 10  -noDepth -processor ../processors/DisparityMapping/libDisparityMapping.so  DisparityMapping -maxFrames 200  -disparitySADWindowSize 25 $@
 
 
 exit 0
