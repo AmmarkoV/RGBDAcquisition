5ab4cc493e39913b5e713176c753e5e403e4262a
Ammar Qammaz <ammarkov@gmail.com>
2015-12-25 00:16:47 +0200

    ...

--- tools/ImageOperations/bilateralFilter.c
+++ tools/ImageOperations/bilateralFilter.c
@@ -1,3 +1,16 @@
 #include "bilateralFilter.h"
 
 
+
+int bilateralFilter(unsigned char * target,  unsigned int targetWidth , unsigned int targetHeight ,
+                    unsigned char * source,  unsigned int sourceWidth , unsigned int sourceHeight ,
+
+                    unsigned char * convolutionMatrix , unsigned int convolutionMatrixWidth , unsigned int convolutionMatrixHeight , unsigned int divisor ,
+
+                    unsigned int tX,  unsigned int tY  ,
+                    unsigned int sX,  unsigned int sY  ,
+                    unsigned int patchWidth , unsigned int patchHeight
+                   )
+{
+ return 0;
+}
