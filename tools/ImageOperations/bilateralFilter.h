4b08679083c05ba1d8bef6fb04afd5bdd6aba779
Ammar Qammaz <ammarkov@gmail.com>
2016-01-04 21:50:41 +0200

    will not try to implement a simple bilateral filter..

--- tools/ImageOperations/bilateralFilter.h
+++ tools/ImageOperations/bilateralFilter.h
@@ -2,5 +2,14 @@
 #define BILATERALFILTER_H_INCLUDED
 
 
+int bilateralFilter(unsigned char * target,  unsigned int targetWidth , unsigned int targetHeight ,
+                    unsigned char * source,  unsigned int sourceWidth , unsigned int sourceHeight ,
+
+                    unsigned char * convolutionMatrix , unsigned int convolutionMatrixWidth , unsigned int convolutionMatrixHeight , unsigned int divisor ,
+
+                    unsigned int tX,  unsigned int tY  ,
+                    unsigned int sX,  unsigned int sY  ,
+                    unsigned int patchWidth , unsigned int patchHeight
+                   );
 
 #endif // BILATERALFILTER_H_INCLUDED
--- tools/ImageOperations/imageOperationsTester.c
+++ tools/ImageOperations/imageOperationsTester.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../Codecs/codecs.h"
 
-int main()
+
+
+int runFilter(char * filenameInput,char * filenameOutput)
 {
-    printf("Hello world!\n");
+ unsigned int inputType = guessFilenameTypeStupid(filenameInput);
+ struct Image * inputImage = readImage(filenameInput,inputType,0);
+ if (inputImage!=0)
+ {
+    unsigned int outputType = guessFilenameTypeStupid(filenameOutput);
+    writeImageFile(inputImage,outputType ,filenameOutput);
+
+    destroyImage(inputImage);
+    return 1;
+ }
+ return 0;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    if (argc<3)
+    {
+      fprintf(stderr,"Not enough arguments\n");
+      return 1;
+    } else
+    if (argc>3)
+    {
+      fprintf(stderr,"Too many arguments\n");
+      return 1;
+    }
+
+    fprintf(stderr,"Converting %s to %s !\n",argv[1],argv[2]);
+      runFilter(argv[1],argv[2]);
     return 0;
 }
