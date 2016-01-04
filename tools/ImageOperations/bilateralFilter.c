7b09004baee4caa976ed619121f9ac4488e3a267
Ammar Qammaz <ammarkov@gmail.com>
2016-01-04 22:40:47 +0200

    ...

--- tools/ImageOperations/bilateralFilter.c
+++ tools/ImageOperations/bilateralFilter.c
@@ -5,14 +5,36 @@
 int bilateralFilter(unsigned char * target,  unsigned int targetWidth , unsigned int targetHeight ,
                     unsigned char * source,  unsigned int sourceWidth , unsigned int sourceHeight ,
 
-                    unsigned char * convolutionMatrix , unsigned int convolutionMatrixWidth , unsigned int convolutionMatrixHeight , unsigned int divisor ,
-
-                    unsigned int tX,  unsigned int tY  ,
-                    unsigned int sX,  unsigned int sY  ,
-                    unsigned int patchWidth , unsigned int patchHeight
+                    float id, float cd , unsigned int dimension
                    )
 {
- return 0;
+  unsigned char * sourcePTR = source;
+  unsigned char * sourceLimit = source+(sourceWidth*sourceHeight*3) ;
+  unsigned char * targetPTR = target;
+  unsigned char * targetLimit = target+(targetWidth*targetHeight*3) ;
+
+
+  if ( dimension != 3 )
+  {
+    fprintf(stderr,"Cannot perform bilateral filter for dimensions other than 3x3\n");
+    return 0;
+  }
+
+  while (sourcePTR < sourceLimit)
+  {
+
+
+
+   sourcePTR+=3;
+  }
+
+
+
+
+
+
+
+ return 1;
 }
 
 
