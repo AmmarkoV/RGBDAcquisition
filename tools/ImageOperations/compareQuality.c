764b9fed22c77e2e95d6f151b9aac55b4a78cc2b
Ammar Qammaz <ammarkov@gmail.com>
2016-01-23 01:10:44 +0200

    ...

--- tools/ImageOperations/compareQuality.c
+++ tools/ImageOperations/compareQuality.c
@@ -1,49 +1,44 @@
 #include "compareQuality.h"
 
 
-inline float absFSub(float  value1,float  value2)
+inline unsigned char absUCPSub(unsigned char * value1,unsigned char * value2)
 {
- if (value1>value2) { return (float ) value1-value2; }
- return (float ) value2-value1;
+ if (*value1>*value2) { return (unsigned char) *value1-*value2; }
+ return (float ) *value2-*value1;
 }
 
 
 float  calculatePSNR(
-                         unsigned char * target,  unsigned int targetWidth , unsigned int targetHeight , unsigned int sourceChannels,
-                         unsigned char * source,  unsigned int sourceWidth , unsigned int sourceHeight , unsigned int targetChannels
-                        )
+                         unsigned char * target,  unsigned int targetWidth , unsigned int targetHeight , unsigned int targetChannels,
+                         unsigned char * source,  unsigned int sourceWidth , unsigned int sourceHeight , unsigned int sourceChannels
+                    )
 {
 
    unsigned char * targetPTR = target;
    unsigned char * sourcePTR = source;
    unsigned char * sourceLimit = source + (sourceWidth * sourceHeight * targetChannels);
 
+   if (
+        (targetWidth!=sourceWidth)  ||
+        (targetHeight!=sourceHeight)  ||
+        (targetChannels!=sourceChannels)
+       )
+   {
+     return 10/0.0;
+   }
 
 
+   unsigned int sum = 0;
    while (sourcePTR < sourceLimit)
    {
-
-    //sq_error=abs(I-J).^2;
-    //psnr = 10*log10( (h*w)  / sum(sq_error(:)) );
-
+    unsigned int absd = absUCPSub(sourcePTR,targetPTR);
+    sum+=absd * absd;
 
      ++sourcePTR;
      ++targetPTR;
    }
 
-/*
-%Input
-%A,B -  Images to evaluate / Size must be equal, double type , values[0 255])
+ float psnr = 10 * log10( (float) (sourceWidth*sourceHeight)  / sum );
 
-[h,w]=size(A);
-
-%Normilize both images
-I=A;%/255;
-J=B;%/255;
-
-
-sq_error=abs(I-J).^2;
-psnr = 10*log10( (h*w)  / sum(sq_error(:)) );
-
-end*/
+ return psnr;
 }
--- tools/ImageOperations/compareQuality.h
+++ tools/ImageOperations/compareQuality.h
@@ -3,8 +3,8 @@
 
 
 float  calculatePSNR(
-                         unsigned char * target,  unsigned int targetWidth , unsigned int targetHeight , unsigned int sourceChannels,
-                         unsigned char * source,  unsigned int sourceWidth , unsigned int sourceHeight , unsigned int targetChannels
-                        );
+                         unsigned char * target,  unsigned int targetWidth , unsigned int targetHeight , unsigned int targetChannels,
+                         unsigned char * source,  unsigned int sourceWidth , unsigned int sourceHeight , unsigned int sourceChannels
+                    );
 
 #endif // COMPAREQUALITY_H_INCLUDED
