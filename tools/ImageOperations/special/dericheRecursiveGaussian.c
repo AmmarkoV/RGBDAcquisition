ef0bdab9d3dea76c8533ced0c05c062571e1fea1
Ammar Qammaz <ammarkov@gmail.com>
2016-01-30 23:57:45 +0200

    deriche gaussian manually templated for floats

--- tools/ImageOperations/special/dericheRecursiveGaussian.c
+++ tools/ImageOperations/special/dericheRecursiveGaussian.c
@@ -6,6 +6,304 @@
 
 
 
+
+
+
+
+inline int deriche1DPassF(
+                   float * source,  unsigned int sourceWidth , unsigned int sourceHeight ,
+                   float * target,  unsigned int targetWidth , unsigned int targetHeight ,
+                   unsigned int x, unsigned int y,
+                   unsigned int direction,
+                   float sigma ,
+                   unsigned int order
+                 )
+{
+ unsigned int i=0;
+ float * sourceLimit = 0;
+ unsigned int offsetToNextPixel=0;
+ unsigned int bufferSize=0;
+
+//Directions are inverted ? ? ?? ?? ? ? ?
+ //First we compute the number of pixels in the line
+ if (direction==0)
+    { //we perform a 1D pass on the X direction
+      //x=0; //We start from X 0
+      sourceLimit = source + ( sourceWidth * y ) + sourceWidth ; //Last Item should have coordinates (sourceWidth-1,y) , so the limit is (sourceWidth,y)
+      offsetToNextPixel=1;    //The way the image is packed we go 1 array space to the right for every access
+      bufferSize=sourceWidth; //The size of our wanted buffer is the same as the width of the image
+    } else
+ if (direction==1)
+    { //we perform a 1D pass on the X direction
+      //y=0; //We start from Y 0
+      sourceLimit = source + ( sourceWidth * (sourceHeight) ) + x; //Last Item should have coordinates (x,sourceHeight-1) , so the limit is (y,sourceHeight)
+      offsetToNextPixel=sourceWidth;     //The way the image is packed we go sourceWidth array space to go down for every access
+      bufferSize=sourceHeight;           //The size of our wanted buffer is the same as the height of the image
+    } else
+    {
+      fprintf(stderr,"Unknown direction for deriche1DPass\n");
+      return 0;
+    }
+
+ //In each of the two cases we want to start at (x,y) position
+ float * sourceStart = source + ( sourceWidth * y ) + x ;
+ float * sourcePTR = sourceStart;
+
+ //These could be moved outside to make the code go softer on syscalls
+ float * y1 = ( float * ) malloc(sizeof(float) * bufferSize);
+ if (y1==0) { fprintf(stderr,"Could not allocate memory for y1 deriche\n"); return 0; }
+ for ( i=0; i<bufferSize; i++ ) { y1[i]=0.0; }
+
+ float * y2 = ( float * ) malloc(sizeof(float) * bufferSize);
+ if (y2==0) { fprintf(stderr,"Could not allocate memory for y2 deriche\n");  free(y1); return 0; }
+ for ( i=0; i<bufferSize; i++ ) { y2[i]=0.0; }
+
+//--- Now we prepare the constants which are applied in the equations
+float alpha = 0.0e+00;
+if ( sigma > 0.0e+00 )
+    {
+      alpha = 1.695e+00/sigma;
+    }
+
+float e =  exp(-alpha);
+float e2 = exp(-2 * alpha);
+
+float p_filter[5]={0};
+float n_filter[5]={0};
+
+float d2,d1,n0,n1,n2,in0,in1,in2,out0,out1,out2,norm;
+// Computing constants that will be applied depending on the order we want
+switch(order)
+ {
+		   case 0 :
+			      norm = (1 - e) * (1 - e) / (1 + 2 * alpha * e - e2);
+
+                  p_filter[0] = -e2;
+			      p_filter[1] = 2 * e;
+			      p_filter[2] = norm;
+			      p_filter[3] = norm * (alpha - 1) * e;
+			      p_filter[4] = 0;
+
+			      n_filter[0] = -e2;
+			      n_filter[1] = 2 * e;
+			      n_filter[2] = 0;
+			      n_filter[3] = norm * (alpha + 1) * e;
+			      n_filter[4] = -norm * e2;
+			      break;
+
+		   case 1 :
+			      norm = (1 - e) * (1 - e) * (1 - e) / 2 / (1 + e);
+
+                  p_filter[0] = -e2;
+			      p_filter[1] = 2 * e;
+			      p_filter[2] = 0;
+			      p_filter[3] = -norm;
+			      p_filter[4] = 0;
+
+                  n_filter[0] = -e2;
+			      n_filter[1] = 2 * e;
+			      n_filter[2] = 0;
+			      n_filter[3] = norm;
+			      n_filter[4] = 0;
+			      break;
+
+		   case 2 :
+			      norm = (1 - e)*(1 - e)*(1 - e)*(1 - e) / (1 + 2 * e - 2 * e * e2 - (e2)*(e2));
+
+                  p_filter[0] = -e2;
+			      p_filter[1] = 2 * e;
+			      p_filter[2] = -norm;
+			      p_filter[3] = norm * (1 + alpha * (1 - e2) / (2 * alpha * e)) * e;
+			      p_filter[4] = 0;
+
+                  n_filter[0] = -e2;
+			      n_filter[1] = 2 * e;
+			      n_filter[2] = 0;
+			      n_filter[3] = -norm * (1 - alpha * (1 - e2) / (2 * alpha * e)) * e;
+			      n_filter[4] = norm * e2;
+			      break;
+
+           default :
+            fprintf(stderr,"Unsupported order ( only signal (0) , first derivative [0] , second derivative [1] supported \n");
+
+           break;
+ };
+
+
+
+// We apply to the one dimensional direction of pixels
+
+d2 = p_filter[0];
+d1 = p_filter[1];
+n0 = p_filter[2];
+n1 = p_filter[3];
+n2 = p_filter[4];   // Note this is always == 0
+
+
+sourcePTR = sourceStart;
+in1 = (float) *sourcePTR;
+in2 = (float) *sourcePTR;
+
+out1 = (n2 + n1 + n0)*in1/(1.0e+00-d1-d2);
+out2 = (n2 + n1 + n0)*in1/(1.0e+00-d1-d2);
+
+for ( i=0; i<bufferSize; i++ )
+{
+	in0  =  (float) *sourcePTR;
+	sourcePTR+=offsetToNextPixel;
+
+	out0 = n2*in2 + n1*in1 + n0*in0 + d1*out1 + d2*out2;
+
+    in2  = in1;
+	in1  = in0;
+	out2 = out1;
+	out1 = out0;
+
+    y1[i] = out0;
+}
+
+
+//
+//--- Now let's apply the Deriche equation (16)
+//
+//    We run right to left accross the line of pixels
+//
+d2 = n_filter[0];
+d1 = n_filter[1];
+n0 = n_filter[2];   // Always == 0
+n1 = n_filter[3];
+n2 = n_filter[4];
+
+
+sourcePTR = sourceLimit-offsetToNextPixel; //We want the last good item
+in1 = (float) *sourcePTR;
+in2 = (float) *sourcePTR;
+
+out1 = (n2 + n1 + n0)*in1 / (1.0e+00 - d1 - d2);
+out2 = (n2 + n1 + n0)*in1 / (1.0e+00 - d1 - d2);
+
+for ( i=(bufferSize-1); i>0; i-- )
+{
+	  in0  =  (float) *sourcePTR;
+	  sourcePTR-=offsetToNextPixel;
+
+      out0 = n2*in2 + n1*in1 + n0*in0 + d1*out1 + d2*out2;
+
+      in2  = in1;
+	  in1  = in0;
+	  out2 = out1;
+	  out1 = out0;
+
+	  y2[i] = out0;
+}
+
+
+// The final result is the summation of the vectors produced by equations 15
+// and 16 of Deriche's paper.
+float yOut;
+float * targetPTR = target + ( ( targetWidth * y ) + x );
+//fprintf(stderr,"source %p , target %p , targetPTR %p  => t(%u,%u) of %u bufs %u \n",source,target,targetPTR,x,y,offsetToNextPixel,bufferSize);
+for (i=0; i<bufferSize; i++)
+{
+  yOut = y1[i] + y2[i];
+  *targetPTR = (float) yOut;
+  //*targetPTR=254;
+  targetPTR+=offsetToNextPixel;
+}
+
+
+ //---- End of procedure
+ free(y1);
+ free(y2);
+
+ return 1;
+}
+
+
+
+
+
+
+
+
+int dericheRecursiveGaussianGrayF(
+                                     float * source,  unsigned int sourceWidth , unsigned int sourceHeight , unsigned int channels,
+                                     float * target,  unsigned int targetWidth , unsigned int targetHeight ,
+                                     float sigma , unsigned int order
+                                   )
+{
+  if (channels!=1)
+  {
+     fprintf(stderr,"dericheRecursiveGaussianGray only accepts grayscale images..\n");
+     return 0;
+  }
+
+  unsigned int x=0,y=0;
+
+     y=0;
+     for (x=0; x<sourceWidth; x++)
+       {
+           deriche1DPassF(
+                          source,  sourceWidth , sourceHeight ,
+                          target , targetWidth , targetHeight ,
+                          x,y,  1, // X direction
+                          sigma , order
+                         );
+       }
+
+     x=0;
+     for (y=0; y<sourceHeight; y++)
+       {
+           deriche1DPassF(
+                          target , targetWidth , targetHeight  ,
+                          target , targetWidth , targetHeight ,
+                          x,y, 0, // Y direction
+                          sigma , order
+                         );
+       }
+
+ return 1;
+}
+
+
+
+
+
+
+/*
+    // - - - - - - - - - - - - - - - - - - - -
+                 FLOAT VERSION
+
+
+
+
+        By the way this is the first thing I made that could be
+          also made using templates .. :P
+
+
+
+
+                 UCHAR VERSION
+    // - - - - - - - - - - - - - - - - - - - -
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 inline int deriche1DPass(
                    unsigned char * source,  unsigned int sourceWidth , unsigned int sourceHeight ,
                    unsigned char * target,  unsigned int targetWidth , unsigned int targetHeight ,
@@ -263,42 +561,4 @@ int dericheRecursiveGaussianGray(
 
 
 
-
-
-int dericheRecursiveGaussianGrayF(
-                                     float * source,  unsigned int sourceWidth , unsigned int sourceHeight , unsigned int channels,
-                                     float * target,  unsigned int targetWidth , unsigned int targetHeight ,
-                                     float sigma , unsigned int order
-                                   )
-{
- if ( (source==0) ) { return 0; }
- if (target==0) { return 0; }
-
- int success=0;
- unsigned char * castedIN = ( unsigned char * ) malloc(sizeof( unsigned char) * sourceWidth*sourceHeight*channels);
- if (castedIN==0) { return 0; }
-
- unsigned char * castedOUT = ( unsigned char * ) malloc(sizeof( unsigned char) * targetWidth*targetHeight*channels);
- if (castedOUT==0) { free(castedIN); return 0; }
-
-
- castFloatImage2UChar(castedIN, source, sourceWidth, sourceHeight , channels);
-
- if (
-     dericheRecursiveGaussianGray(
-                                  castedIN,  sourceWidth , sourceHeight , channels,
-                                  castedOUT,  targetWidth , targetHeight ,
-                                  sigma , order
-                                )
-    )
-    {
-      castUCharImage2Float(target , castedOUT , targetWidth, targetHeight , channels);
-      success=1;
-    }
-
-
- free(castedIN);
- free(castedOUT);
- return success;
-}
 
--- tools/ImageOperations/special/dericheRecursiveGaussian.h
+++ tools/ImageOperations/special/dericheRecursiveGaussian.h
@@ -1,16 +1,19 @@
 #ifndef DERICHERECURSIVEGAUSSIAN_H_INCLUDED
 #define DERICHERECURSIVEGAUSSIAN_H_INCLUDED
 
-int dericheRecursiveGaussianGray(
-                                  unsigned char * source,  unsigned int sourceWidth , unsigned int sourceHeight , unsigned int channels,
-                                  unsigned char * target,  unsigned int targetWidth , unsigned int targetHeight ,
-                                  float sigma , unsigned int order
-                                );
 
 int dericheRecursiveGaussianGrayF(
                                      float * source,  unsigned int sourceWidth , unsigned int sourceHeight , unsigned int channels,
                                      float * target,  unsigned int targetWidth , unsigned int targetHeight ,
                                      float sigma , unsigned int order
                                    );
+
+
+int dericheRecursiveGaussianGray(
+                                  unsigned char * source,  unsigned int sourceWidth , unsigned int sourceHeight , unsigned int channels,
+                                  unsigned char * target,  unsigned int targetWidth , unsigned int targetHeight ,
+                                  float sigma , unsigned int order
+                                );
+
 
 #endif // DERICHERECURSIVEGAUSSIAN_H_INCLUDED
