e49c5aabdf2befc302eb99f2b5cb6c598e28619e
Ammar Qammaz <ammarkov@gmail.com>
2016-02-08 13:53:14 +0200

    ...

--- grabber/frames/compress.sh
+++ grabber/frames/compress.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR" 
 
 mv $1 $1-Uncompressed
-./convertPNMDatasetToJPGPNG.sh $1 $2
+./convertPNMDatasetToJPGPNG.sh $1-Uncompressed $1
 
 exit 0
