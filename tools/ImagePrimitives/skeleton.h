71c23caaa76860fcf3d983d3233df41e8cd57f06
Ammar Qammaz <ammarkov@gmail.com>
2016-02-11 18:39:31 +0200

    skeleton structure now has the bounding box function on it too

--- tools/ImagePrimitives/skeleton.h
+++ tools/ImagePrimitives/skeleton.h
@@ -3,24 +3,56 @@
 
 
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
 static const char * jointNames[] =
-{"head",
- "neck",
+{
+ "head",
+ "neck" ,
  "torso",
- "left_shoulder",
  "right_shoulder",
- "left_elbow",
+ "left_shoulder",
  "right_elbow",
- "left_hand",
+ "left_elbow",
  "right_hand",
- "left_hip",
+ "left_hand",
  "right_hip",
- "left_knee",
+ "left_hip",
  "right_knee",
- "left_foot",
- "right_foot"
+ "left_knee",
+ "right_foot",
+ "left_foot" ,
+ "hip" ,
+ //=================
+ "End of Joint Names"
 };
 
+
+static const char * tgbtNames[] =
+{
+ "head",
+ "neck",
+ "bodyCenter",
+ "rightShoulder",
+ "leftShoulder",
+ "rightElbow",
+ "leftElbow",
+ "rightWrist",
+ "leftWrist",
+ "rightLegRoot",
+ "leftLegRoot",
+ "rightKnee",
+ "leftKnee",
+ "rightAnkle",
+ "leftAnkle",
+ "hip",
+ //--------------------
+ "End Of TGBT Names"
+};
 
 static const char * const humanSkeletonJointNames[] =
     {
@@ -38,7 +70,8 @@ static const char * const humanSkeletonJointNames[] =
        "HUMAN_SKELETON_RIGHT_KNEE",
        "HUMAN_SKELETON_LEFT_KNEE",
        "HUMAN_SKELETON_RIGHT_FOOT",
-       "HUMAN_SKELETON_LEFT_FOOT"
+       "HUMAN_SKELETON_LEFT_FOOT",
+       "HUMAN_SKELETON_HIP"
     };
 
 enum humanSkeletonJoints
@@ -58,9 +91,32 @@ enum humanSkeletonJoints
    HUMAN_SKELETON_LEFT_KNEE,
    HUMAN_SKELETON_RIGHT_FOOT,
    HUMAN_SKELETON_LEFT_FOOT,
+   HUMAN_SKELETON_HIP,
    //---------------------
    HUMAN_SKELETON_PARTS
 };
+
+
+static const int humanSkeletonJointsRelationMap[] =
+{ // Parent                        Joint
+  HUMAN_SKELETON_HEAD,           //HUMAN_SKELETON_HEAD
+  HUMAN_SKELETON_HEAD,           //HUMAN_SKELETON_NECK
+  HUMAN_SKELETON_NECK,           //HUMAN_SKELETON_TORSO
+  HUMAN_SKELETON_NECK,           //HUMAN_SKELETON_RIGHT_SHOULDER
+  HUMAN_SKELETON_NECK,           //HUMAN_SKELETON_LEFT_SHOULDER
+  HUMAN_SKELETON_RIGHT_SHOULDER, //HUMAN_SKELETON_RIGHT_ELBOW
+  HUMAN_SKELETON_LEFT_SHOULDER,  //HUMAN_SKELETON_LEFT_ELBOW
+  HUMAN_SKELETON_RIGHT_ELBOW,    //HUMAN_SKELETON_RIGHT_HAND
+  HUMAN_SKELETON_LEFT_ELBOW,     //HUMAN_SKELETON_LEFT_HAND
+  HUMAN_SKELETON_HIP,            //HUMAN_SKELETON_RIGHT_HIP
+  HUMAN_SKELETON_HIP,            //HUMAN_SKELETON_LEFT_HIP
+  HUMAN_SKELETON_RIGHT_HIP,      //HUMAN_SKELETON_RIGHT_KNEE
+  HUMAN_SKELETON_LEFT_HIP,       //HUMAN_SKELETON_LEFT_KNEE
+  HUMAN_SKELETON_LEFT_KNEE,      //HUMAN_SKELETON_RIGHT_FOOT
+  HUMAN_SKELETON_LEFT_KNEE,      //HUMAN_SKELETON_LEFT_FOOT
+  HUMAN_SKELETON_TORSO,          //HUMAN_SKELETON_HIP
+};
+
 
 static const char * const humanSkeletonMirroredJointNames[] =
     {
@@ -79,6 +135,7 @@ static const char * const humanSkeletonMirroredJointNames[] =
       "HUMAN_SKELETON_MIRRORED_RIGHT_KNEE",
       "HUMAN_SKELETON_MIRRORED_LEFT_FOOT",
       "HUMAN_SKELETON_MIRRORED_RIGHT_FOOT",
+      "HUMAN_SKELETON_MIRRORED_HIP"
     };
 
 enum humanMirroredSkeletonJoints
@@ -98,22 +155,21 @@ enum humanMirroredSkeletonJoints
    HUMAN_SKELETON_MIRRORED_RIGHT_KNEE,
    HUMAN_SKELETON_MIRRORED_LEFT_FOOT,
    HUMAN_SKELETON_MIRRORED_RIGHT_FOOT,
+   HUMAN_SKELETON_MIRRORED_HIP,
    //---------------------
    HUMAN_SKELETON_MIRRORED_PARTS
 };
 
-extern const char * jointNames[];
-
-struct point3D
-{
-    float x,y,z;
-};
-
+   
 struct point2D
 {
     float x,y;
 };
 
+struct point3D
+{
+    float x,y,z;
+};
 
 struct skeletonHuman
 {
@@ -141,6 +197,53 @@ struct skeletonPointing
   unsigned char isRightHand;
 };
 
+
+
+static void updateSkeletonBoundingBox(struct skeletonHuman * sk)
+{
+  //Use joints to extract bbox
+  float minX = sk->joint[HUMAN_SKELETON_HEAD].x; float maxX = sk->joint[HUMAN_SKELETON_HEAD].x;
+  float minY = sk->joint[HUMAN_SKELETON_HEAD].y; float maxY = sk->joint[HUMAN_SKELETON_HEAD].y;
+  float minZ = sk->joint[HUMAN_SKELETON_HEAD].z; float maxZ = sk->joint[HUMAN_SKELETON_HEAD].z;
+
+  unsigned int i=0;
+     for (i=0; i<HUMAN_SKELETON_PARTS; i++)
+      {
+       if (
+            (sk->joint[i].x!=0.0) ||
+            (sk->joint[i].y!=0.0) ||
+            (sk->joint[i].z!=0.0)
+           )
+        {
+         if (sk->joint[i].x>maxX) { maxX = sk->joint[i].x; } else
+         if (sk->joint[i].x<minX) { minX = sk->joint[i].x; }
+
+         if (sk->joint[i].y>maxY) { maxY = sk->joint[i].y; } else
+         if (sk->joint[i].y<minY) { minY = sk->joint[i].y; }
+
+         if (sk->joint[i].z>maxZ) { maxZ = sk->joint[i].z; } else
+         if (sk->joint[i].z<minZ) { minZ = sk->joint[i].z; }
+        }
+      }
+
+     sk->bbox[0].x = maxX; sk->bbox[0].y = maxY; sk->bbox[0].z = minZ;
+     sk->bbox[1].x = maxX; sk->bbox[1].y = minY; sk->bbox[1].z = minZ;
+     sk->bbox[2].x = minX; sk->bbox[2].y = minY; sk->bbox[2].z = minZ;
+     sk->bbox[3].x = minX; sk->bbox[3].y = maxY; sk->bbox[3].z = minZ;
+     sk->bbox[4].x = maxX; sk->bbox[4].y = maxY; sk->bbox[4].z = maxZ;
+     sk->bbox[5].x = maxX; sk->bbox[5].y = minY; sk->bbox[5].z = maxZ;
+     sk->bbox[6].x = minX; sk->bbox[6].y = minY; sk->bbox[6].z = maxZ;
+     sk->bbox[7].x = minX; sk->bbox[7].y = maxY; sk->bbox[7].z = maxZ;
+
+     sk->bboxDimensions.x = (float) sk->bbox[4].x-sk->bbox[2].x;
+     sk->bboxDimensions.y = (float) sk->bbox[4].y-sk->bbox[2].y;
+     sk->bboxDimensions.z = (float) sk->bbox[4].z-sk->bbox[2].z; 
+}
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // SKELETON_H_INCLUDED
 
