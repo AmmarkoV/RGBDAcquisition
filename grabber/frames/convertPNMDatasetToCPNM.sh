adc6deff5d4ce05981d0ee44ff055981a3329748
Ammar Qammaz <ammarkov@gmail.com>
2016-02-15 14:57:14 +0200

    ..

---
+++ grabber/frames/convertPNMDatasetToCPNM.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+  
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR" 
+
+cd $1
+
+mkdir ../$2
+
+cp *.calib ../$2/
+cp *.sh ../$2/
+
+echo "Converting Color Files"
+
+FILES_TO_CONVERT=`ls | grep color`
+for f in $FILES_TO_CONVERT
+do 
+ TARGETNAME=`basename $f .pnm`
+ mv  $f ../$2/$TARGETNAME.cpnm
+done
+
+
+echo "Converting Depth Files"
+
+FILES_TO_CONVERT=`ls | grep depth`
+for f in $FILES_TO_CONVERT
+do 
+ TARGETNAME=`basename $f .pnm`
+ ../../../tools/DepthImagesConverter/DepthImagesConverter "$f" "../$2/$TARGETNAME.cpnm"
+done
+
+cd ..
+
+exit 0
--- tools/DepthImagesConverter/main.cpp
+++ tools/DepthImagesConverter/main.cpp
@@ -132,6 +132,21 @@ int readFromPNMDepthAndWriteToPNGDepth(char * inputFilename,char *outputFilename
 
 
 
+int readFromPNMDepthAndWriteToCPNMDepth(char * inputFilename,char *outputFilename)
+{
+    struct Image * newImg  = readImage(inputFilename,PNM_CODEC,0);
+    if (newImg==0) { fprintf(stderr,"Could not open %s\n",inputFilename); return 0; }
+
+    fprintf(stderr,"Swapping Loaded Image with width : %u , height %u , channels %u , bitsperpixel %u\n",newImg->width,newImg->height,newImg->channels,newImg->bitsperpixel);
+
+    writeImageFile(newImg,COMPATIBLE_PNM_CODEC,outputFilename);
+
+   return 0;
+}
+
+
+
+
 int main( int argc, char** argv )
 {
 
@@ -155,6 +170,11 @@ int main( int argc, char** argv )
      {
        fprintf(stderr,"Using my custom loader / writer \n");
        return readFromPNMDepthAndWriteToPNGDepth(argv[1],argv[2]);
+     }else
+     if ( (strstr(argv[1],".pnm")!=0) && (strstr(argv[2],".cpnm")!=0) )
+     {
+       fprintf(stderr,"Using my custom loader / writer \n");
+       return readFromPNMDepthAndWriteToCPNMDepth(argv[1],argv[2]);
      }
 
     }
