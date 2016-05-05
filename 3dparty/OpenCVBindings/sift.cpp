73ddd1f71511064377b856963981c113bfb4937c
Ammar Qammaz <ammarkov@gmail.com>
2016-05-06 02:56:32 +0300

    adding a 3dparty binding for OpenCV to facilitate SIFT etc..

---
+++ 3dparty/OpenCVBindings/sift.cpp
@@ -0,0 +1,19 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/nonfree/features2d.hpp> //Thanks to Alessandro
+
+int main(int argc, const char* argv[])
+{
+    const cv::Mat input = cv::imread("input.jpg", 0); //Load as grayscale
+
+    cv::SiftFeatureDetector detector;
+    std::vector<cv::KeyPoint> keypoints;
+    detector.detect(input, keypoints);
+
+    // Add results to image and save.
+    cv::Mat output;
+    cv::drawKeypoints(input, keypoints, output);
+    cv::imwrite("sift_result.jpg", output);
+
+    return 0;
+}
