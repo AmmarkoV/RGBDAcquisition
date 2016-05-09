edc585ed7f16ed5291640ab0aaef57a1e04ff011
Ammar Qammaz <ammarkov@gmail.com>
2016-05-09 20:21:46 +0300

    almost working..

--- 3dparty/OpenCVBindings/sift.cpp
+++ 3dparty/OpenCVBindings/sift.cpp
@@ -12,7 +12,7 @@
 #include <string.h>
 #include <unistd.h>
 
-const double THRESHOLD = 380;
+const double THRESHOLD = 350;
 
 
 void clear_line()
@@ -60,7 +60,7 @@ void findPairs(std::vector<cv::KeyPoint>& keypoints1, cv::Mat& descriptors1,
   for (int i = 0; i < descriptors1.rows; i++)
   {
     clear_line();
-    fprintf(stderr,"Checking pairs , threshold %0.2f : \n",THRESHOLD);
+    fprintf(stderr,"Checking SIFT pairs , threshold %0.2f : \n",THRESHOLD);
     fprintf(stderr,"%u / %u checks  - %u matches \n",i, descriptors1.rows , srcPoints.size());
     cv::KeyPoint pt1 = keypoints1[i];
     cv::Mat desc1 = descriptors1.row(i);
@@ -127,16 +127,6 @@ int visualizeMatches(
     }
 
 
-    // Draw line between nearest neighbor pairs
-    for (unsigned int i = 0; i < (int)srcPoints.size(); ++i)
-    {
-      cv::Point2f pt1 = srcPoints[i];
-      cv::Point2f pt2 = dstPoints[i];
-      cv::Point2f from = pt1;
-      cv::Point2f to   = cv::Point(size.width + pt2.x, size.height + pt2.y);
-      cv::line(matchingImage, from, to, cv::Scalar(0, 0 , 255));
-    }
-
 if (srcRANSACPoints.size()>0)
 {    // Draw line between ransac neighbor pairs
     for (int i = 0; i < (int)srcRANSACPoints.size(); ++i)
@@ -147,6 +137,17 @@ if (srcRANSACPoints.size()>0)
       cv::Point2f to   = cv::Point(size.width + pt2.x, size.height + pt2.y);
       cv::line(matchingImage, from, to, cv::Scalar(0, 255, 255));
     }
+} else
+{
+    // Draw line between nearest neighbor pairs
+    for (unsigned int i = 0; i < (int)srcPoints.size(); ++i)
+    {
+      cv::Point2f pt1 = srcPoints[i];
+      cv::Point2f pt2 = dstPoints[i];
+      cv::Point2f from = pt1;
+      cv::Point2f to   = cv::Point(size.width + pt2.x, size.height + pt2.y);
+      cv::line(matchingImage, from, to, cv::Scalar(0, 0 , 255));
+    }
 }
 
     // Display mathing image
@@ -156,12 +157,33 @@ if (srcRANSACPoints.size()>0)
 }
 
 
+void checkAffineSolution ( double * outX ,double * outY , double * dstX,double * dstY , double * M , double * srcX, double * srcY)
+{
+  double a=M[0] , b=M[1] , c=M[2];
+  double d=M[3] , e=M[4] , f=M[5];
+
+  *outX = c + ( (double) a * *srcX ) + ((double) b * *srcY );
+  *outY = f + ( (double) d * *srcX ) + ((double) e * *srcY );
+
+
+   if ( *outX > *dstX ) { *outX = *outX - *dstX; } else { *outX = *dstX - *outX; }
+   if ( *outY > *dstY ) { *outY = *outY - *dstY; } else { *outY = *dstY - *outY; }
+
+}
+
 int checkAffineFitness(
                          double * M ,
                          std::vector<cv::Point2f> &srcPoints ,
-                         std::vector<cv::Point2f> &dstPoints
+                         std::vector<cv::Point2f> &dstPoints ,
+                         double * avgX ,
+                         double * avgY ,
+                         std::vector<cv::Point2f> &srcRANSACPoints ,
+                         std::vector<cv::Point2f> &dstRANSACPoints
                       )
 {
+  srcRANSACPoints.clear();
+  dstRANSACPoints.clear();
+  *avgX = 0; *avgY = 0;
 
   // { { c a * sx + b sy - dx } ,  { f d sx + e sy - dy } } = 0
   unsigned int inlierCount=0;
@@ -173,34 +195,50 @@ int checkAffineFitness(
   double sumX=0,sumY=0;
   double sx=0.0 , sy=0.0 ,  dx=0.0 , dy=0.0 , rx=0.0 , ry=0.0;
 
+
+
+
   for (i=0; i<srcPoints.size(); i++)
   {
     sx = (double) srcPoints[i].x;  sy = (double) srcPoints[i].y;
     dx = (double) dstPoints[i].x;  dy = (double) dstPoints[i].y;
 
-    rx = ( c + ( a * sx ) + ( b * sy ) ) ;
-    ry = ( f + ( d * sx ) + ( e * sy ) ) ;
+    rx = (double) c + ( a * sx ) + ( b * sy )  ;
+    ry = (double) f + ( d * sx ) + ( e * sy )  ;
 
-    //fprintf(stderr,"diff  %0.2f %0.2f vs %0.2f %0.2f  = ",rx,ry,dx,dy);
-    rx = abs(rx-dx); ry = abs(ry-dy);
+    if ( rx > dx ) { rx = rx - dx; } else { rx = dx - rx; }
+    if ( ry > dy ) { ry = ry - dy; } else { ry = dy - ry; }
     //fprintf(stderr," %0.2f %0.2f \n",rx,ry);
 
-    sumX+=rx; sumY+=ry;
 
-    if ( (rx<100)&&(ry<100) )
+    if ( (rx<5)&&(ry<5) )
     {
+       sumX+=rx; sumY+=ry;
        ++inlierCount;
+
+      cv::Point2f srcPT; srcPT.x = sx; srcPT.y = sy;
+      cv::Point2f dstPT; dstPT.x = dx; dstPT.y = dy;
+      srcRANSACPoints.push_back(srcPT);
+      dstRANSACPoints.push_back(dstPT);
     }
   }
 
-  fprintf(stderr,"{ { a=%0.2f , b=%0.2f , c=%0.2f } , { d=%0.2f , e=%0.2f , f=%0.2f } }  ",a,b,c,d,e,f);
-  fprintf(stderr,"%u inliers / %u points  , average ( %0.2f,%0.2f ) \n",inlierCount , srcPoints.size() , (float)sumX/srcPoints.size() , (float)sumY/srcPoints.size()  );
+ if (inlierCount>0)
+ {
+  *avgX = (double) sumX/inlierCount;
+  *avgY = (double) sumY/inlierCount;
+  }
+
+  //fprintf(stderr,"{ { a=%0.2f , b=%0.2f , c=%0.2f } , { d=%0.2f , e=%0.2f , f=%0.2f } }  ",a,b,c,d,e,f);
+  //fprintf(stderr,"%u inliers / %u points  , average ( %0.2f,%0.2f ) \n",inlierCount , srcPoints.size() , *avgX , *avgY );
   return inlierCount;
 }
 
 
 int fitAffineTransformationMatchesRANSAC(
                                           unsigned int loops ,
+                                          double * M ,
+                                          cv::Mat & warp_mat ,
                                           std::vector<cv::Point2f> &srcPoints ,
                                           std::vector<cv::Point2f> &dstPoints ,
                                           std::vector<cv::Point2f> &srcRANSACPoints ,
@@ -214,7 +252,7 @@ int fitAffineTransformationMatchesRANSAC(
   //
   // DST | x |  = M | a b c | * SRC | x |
   //     | y |      | d e f |       | y |
-  //                               | 1 |
+  //                                | 1 |
   //{ {dx} , {dy } } = { { a ,b, c} , { d , e ,f  } } . { { sx } , { sy } , { 1 } }
   //{ {dx} , {dy } } = { { c a * sx + b sy } ,  { f d sx + e sy } }
   // { { c a * sx + b sy - dx } ,  { f d sx + e sy - dy } } = 0
@@ -223,15 +261,22 @@ int fitAffineTransformationMatchesRANSAC(
 
   unsigned int ptA=0,ptB=0,ptC=0;
 
-  double M[6]={0};
+
+  std::vector<cv::Point2f> bestSrcRANSACPoints;
+  std::vector<cv::Point2f> bestDstRANSACPoints;
+
   cv::Point2f srcTri[3];
   cv::Point2f dstTri[3];
-  cv::Mat warp_mat( 2, 3,  CV_64FC1  );
+
+
+  cv::Mat bestWarp_mat( 2, 3,  CV_64FC1  );
+  double bestM[6]={0};
+  unsigned int bestInliers=0;
+  double avgX,avgY,bestAvgX=66660.0,bestAvgY=66660.0;
 
   unsigned int i=0,z=0;
   for (i=0; i<loops; i++)
   {
-   fprintf(stderr," ____________________________________________________________ \n");
     //RANSAC pick 3 points
     ptA=rand()%srcPoints.size();
     ptB=rand()%srcPoints.size();
@@ -249,33 +294,83 @@ int fitAffineTransformationMatchesRANSAC(
    /// Get the Affine Transform
    //derive resultMatrix with Gauss Jordan
    warp_mat = cv::getAffineTransform( srcTri, dstTri );
+   //fprintf(stderr," ____________________________________________________________ \n");
    //std::cout << warp_mat<<"\n";
 
    M[0] = warp_mat.at<double>(0,0); M[1] = warp_mat.at<double>(0,1); M[2] = warp_mat.at<double>(0,2);
    M[3] = warp_mat.at<double>(1,0); M[4] = warp_mat.at<double>(1,1); M[5] = warp_mat.at<double>(1,2);
 
-
+  /*
    for (z=0; z<3; z++)
    {
     fprintf(stderr,"{ { %0.2f } , { %0.2f } }  = ",dstTri[z].x,dstTri[z].y);
     fprintf(stderr,"{ { %0.2f , %0.2f , %0.2f } , { %0.2f , %0.2f , %0.2f } }  ",M[0],M[1],M[2],M[3],M[4],M[5]);
     fprintf(stderr," . { { %0.2f } , { %0.2f } , { 1 } }  \n\n",srcTri[z].x,srcTri[z].y);
+
+
+     double outX , outY , dstX = dstTri[z].x , dstY = dstTri[z].y , srcX = srcTri[z].x , srcY= srcTri[z].y;
+     checkAffineSolution ( &outX , &outY , &dstX, &dstY , M , &srcX, &srcY);
+
+     fprintf(stderr,"diff is %0.2f , %0.2f\n",outX , outY);
    }
+  */
 
 
-   checkAffineFitness( M , srcPoints , dstPoints );
+   unsigned int inliers = checkAffineFitness( M , srcPoints , dstPoints , &avgX, &avgY , srcRANSACPoints , dstRANSACPoints );
+
+   if (inliers > bestInliers )
+    {
+      bestInliers = inliers;
+      bestAvgX = avgX;
+      bestAvgY = avgY;
+      for (z=0; z<6; z++) { bestM[z]=M[z]; }
+      bestSrcRANSACPoints=srcRANSACPoints;
+      bestDstRANSACPoints=dstRANSACPoints;
+      bestWarp_mat = warp_mat;
+    }
+
+
+    clear_line();
+    fprintf(stderr,"RANSACing affine transform , %u / %u loops : \n",i,loops);
+    fprintf(stderr,"Best : %u inliers  , avg ( %0.2f , %0.2f )  \n",bestInliers,bestAvgX,bestAvgY);
+
+
+    for (z=0; z<6; z++) { M[z]=bestM[z]; }
+    srcRANSACPoints=bestSrcRANSACPoints;
+    dstRANSACPoints= bestDstRANSACPoints;
+    warp_mat = bestWarp_mat;
+
 
   }
 
-  fprintf(stderr,"fitAffineTransformationMatchesRANSAC done \n");
+  //fprintf(stderr,"fitAffineTransformationMatchesRANSAC done got %u inliers with avg ( %0.2f , %0.2f ) \n",bestInliers,bestAvgX,bestAvgY);
   return 1;
 }
 
 
 
+int stitchMatch(
+                const char * filenameOutput ,
+                cv::Mat & left ,
+                cv::Mat & right ,
+                cv::Mat & warp_mat
+               )
+{
+   /// Apply the Affine Transform just found to the src image
+   cv::Mat warp_dst = cv::Mat::zeros( left.rows, left.cols, left.type() );
+   warpAffine( left, warp_dst, warp_mat, warp_dst.size() );
+   right.copyTo(warp_dst);
+
+   double alpha = 0.5; double beta = ( 1.0 - alpha );
+   addWeighted( warp_dst, alpha, right, beta, 0.0, warp_dst);
+
+   cv::imwrite( filenameOutput  , warp_dst);
+
+}
 
 int main(int argc, const char* argv[])
 {
+
     cv::Mat left = cv::imread("uttower_left.JPG"  , CV_LOAD_IMAGE_COLOR);
     if(! left.data ) { fprintf(stderr,"Left Image missing \n"); return 1; }
 
@@ -303,7 +398,7 @@ int main(int argc, const char* argv[])
     cv::drawKeypoints(right, keypointsRight, output);
     cv::imwrite("sift_result_right.jpg", output);
 
-    fprintf(stderr,"SIFT features ready \n");
+    //fprintf(stderr,"SIFT features ready \n");
 
 
     std::vector<cv::Point2f> srcRANSACPoints;
@@ -313,7 +408,7 @@ int main(int argc, const char* argv[])
     std::vector<cv::Point2f> srcPoints;
     std::vector<cv::Point2f> dstPoints;
     findPairs(keypointsLeft, descriptorsLeft, keypointsRight, descriptorsRight, srcPoints, dstPoints);
-    printf("%zd keypoints are matched.\n", srcPoints.size());
+    //printf("%zd keypoints are matched.\n", srcPoints.size());
 
 
 
@@ -333,7 +428,18 @@ int main(int argc, const char* argv[])
 
 
 
-   fitAffineTransformationMatchesRANSAC( 1000, srcPoints , dstPoints ,  srcRANSACPoints, dstRANSACPoints);
+   cv::Mat warp_mat( 2, 3,  CV_64FC1  );
+   double M[6]={0};
+   fitAffineTransformationMatchesRANSAC( 1000, M , warp_mat, srcPoints , dstPoints ,  srcRANSACPoints, dstRANSACPoints);
+
+
+   stitchMatch(
+                "wrapped.jpg"  ,
+                 left ,
+                 right ,
+                 warp_mat
+               );
+
 
    visualizeMatches(
                       "sift_affine_match.jpg" ,
