f6a68c31293d8bacdbecdd7302a5528236e22d41
Ammar Qammaz <ammarkov@gmail.com>
2016-05-27 20:06:41 +0300

    ...

--- 3dparty/OpenCVBindings/make.sh
+++ 3dparty/OpenCVBindings/make.sh
@@ -5,17 +5,17 @@ CDPATH="."
 mkdir -p obj/Debug
 
 g++ -Wall -fexceptions -fPIC -I/usr/include/opencv -g  -c $CDPATH/affine.cpp -o obj/Debug/affine.o
+g++ -Wall -fexceptions -fPIC -I/usr/include/opencv -g  -c $CDPATH/fundamental.cpp -o obj/Debug/fundamental.o
 g++ -Wall -fexceptions -fPIC -I/usr/include/opencv -g  -c $CDPATH/homography.cpp -o obj/Debug/homography.o
 g++ -Wall -fexceptions -fPIC -I/usr/include/opencv -g  -c $CDPATH/panorama.cpp -o obj/Debug/panorama.o
+g++ -Wall -fexceptions -fPIC -I/usr/include/opencv -g  -c $CDPATH/reconstruction.cpp -o obj/Debug/reconstruction.o
 g++ -Wall -fexceptions -fPIC -I/usr/include/opencv -g  -c $CDPATH/sift.cpp  -o obj/Debug/sift.o 
 g++ -Wall -fexceptions -fPIC -I/usr/include/opencv -g  -c $CDPATH/stitcher.cpp -o obj/Debug/stitcher.o
 g++ -Wall -fexceptions -fPIC -I/usr/include/opencv -g  -c $CDPATH/tools.cpp -o obj/Debug/tools.o
 
-g++  -o panorama obj/Debug/affine.o obj/Debug/homography.o obj/Debug/panorama.o obj/Debug/sift.o obj/Debug/stitcher.o obj/Debug/tools.o  /usr/lib/x86_64-linux-gnu/libopencv_calib3d.so /usr/lib/x86_64-linux-gnu/libopencv_contrib.so /usr/lib/x86_64-linux-gnu/libopencv_core.so /usr/lib/x86_64-linux-gnu/libopencv_features2d.so /usr/lib/x86_64-linux-gnu/libopencv_flann.so /usr/lib/x86_64-linux-gnu/libopencv_gpu.so /usr/lib/x86_64-linux-gnu/libopencv_highgui.so /usr/lib/x86_64-linux-gnu/libopencv_imgproc.so /usr/lib/x86_64-linux-gnu/libopencv_legacy.so /usr/lib/x86_64-linux-gnu/libopencv_ml.so /usr/lib/x86_64-linux-gnu/libopencv_nonfree.so /usr/lib/x86_64-linux-gnu/libopencv_objdetect.so /usr/lib/x86_64-linux-gnu/libopencv_ocl.so /usr/lib/x86_64-linux-gnu/libopencv_photo.so /usr/lib/x86_64-linux-gnu/libopencv_stitching.so /usr/lib/x86_64-linux-gnu/libopencv_superres.so /usr/lib/x86_64-linux-gnu/libopencv_ts.so /usr/lib/x86_64-linux-gnu/libopencv_video.so /usr/lib/x86_64-linux-gnu/libopencv_videostab.so -lopencv_calib3d -lopencv_contrib -lopencv_core -lopencv_features2d -lopencv_flann -lopencv_gpu -lopencv_highgui -lopencv_imgproc -lopencv_legacy -lopencv_ml -lopencv_nonfree -lopencv_objdetect -lopencv_ocl -lopencv_photo -lopencv_stitching -lopencv_superres -lopencv_ts -lopencv_video -lopencv_videostab   
+g++  -o panorama obj/Debug/affine.o obj/Debug/fundamental.o obj/Debug/homography.o obj/Debug/panorama.o obj/Debug/reconstruction.o obj/Debug/sift.o obj/Debug/stitcher.o obj/Debug/tools.o /usr/local/lib/libopencv_calib3d.so /usr/local/lib/libopencv_contrib.so /usr/local/lib/libopencv_core.so /usr/local/lib/libopencv_features2d.so /usr/local/lib/libopencv_flann.so /usr/local/lib/libopencv_gpu.so /usr/local/lib/libopencv_highgui.so /usr/local/lib/libopencv_imgproc.so /usr/local/lib/libopencv_legacy.so /usr/local/lib/libopencv_ml.so /usr/local/lib/libopencv_nonfree.so /usr/local/lib/libopencv_objdetect.so /usr/local/lib/libopencv_photo.so /usr/local/lib/libopencv_stitching.so /usr/local/lib/libopencv_superres.so /usr/local/lib/libopencv_ts.a /usr/local/lib/libopencv_video.so /usr/local/lib/libopencv_videostab.so -lrt -lpthread -lm -ldl 
 
-
-
-
-
+ 
+ 
 
 exit 0
