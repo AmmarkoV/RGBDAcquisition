a28e093d77e9a2f426c941d92f0b3f03df08c144
Ammar Qammaz <ammarkov@gmail.com>
2016-06-08 05:05:49 +0300

    ...

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/ModelLoader/assimp_loader.cpp
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/ModelLoader/assimp_loader.cpp
@@ -260,140 +260,6 @@ void transMesh(struct aiScene *scene , int meshNumber , struct TRI_Model * index
 }
 
 
-
-
-
-
-
-void prepareFlatMesh(struct aiScene *scene , int meshNumber , struct TRI_Model * triModel )
-{
-    struct aiMesh * mesh = scene->mMeshes[meshNumber];
-
-    fprintf(stderr,"Preparing mesh %u   \n",meshNumber);
-    fprintf(stderr,"  %u vertices \n",mesh->mNumVertices);
-    fprintf(stderr,"  %u normals \n",mesh->mNumVertices);
-    fprintf(stderr,"  %d faces \n",mesh->mNumFaces);
-
-	unsigned int k;
-
-    fprintf(stderr,"  %d bones\n",mesh->mNumBones);
-	for (k = 0; k < mesh->mNumBones; k++)
-    {
-		aiString boneName = mesh->mBones[k]->mName;
-		fprintf(stderr,"   -bone %u - %s \n" , k , boneName.C_Str() );
-		fprintf(stderr,"   | %0.2f  | %0.2f  | %0.2f  | %0.2f  | \n" , mesh->mBones[k]->mOffsetMatrix[0], mesh->mBones[k]->mOffsetMatrix[1], mesh->mBones[k]->mOffsetMatrix[2], mesh->mBones[k]->mOffsetMatrix[3] );
-		fprintf(stderr,"   | %0.2f  | %0.2f  | %0.2f  | %0.2f  | \n" , mesh->mBones[k]->mOffsetMatrix[4], mesh->mBones[k]->mOffsetMatrix[5], mesh->mBones[k]->mOffsetMatrix[6], mesh->mBones[k]->mOffsetMatrix[7] );
-		fprintf(stderr,"   | %0.2f  | %0.2f  | %0.2f  | %0.2f  | \n" , mesh->mBones[k]->mOffsetMatrix[8], mesh->mBones[k]->mOffsetMatrix[9], mesh->mBones[k]->mOffsetMatrix[10],mesh->mBones[k]->mOffsetMatrix[11] );
-		fprintf(stderr,"   | %0.2f  | %0.2f  | %0.2f  | %0.2f  | \n" , mesh->mBones[k]->mOffsetMatrix[12],mesh->mBones[k]->mOffsetMatrix[13], mesh->mBones[k]->mOffsetMatrix[14], mesh->mBones[k]->mOffsetMatrix[15] );
-	}
-
-
-    fprintf(stderr,"%u color sets",AI_MAX_NUMBER_OF_COLOR_SETS);
-    unsigned int colourSet = 0;
-        for (colourSet = 0; colourSet< AI_MAX_NUMBER_OF_COLOR_SETS; colourSet++)
-        {
-          fprintf(stderr," c%u ",colourSet);
-        }
-    fprintf(stderr," \n");
-
-    fprintf(stderr,"Preparing mesh %u with %u colors \n",meshNumber,mesh->mNumFaces,meshNumber);
-	//xxxmesh->texture = loadmaterial(scene->mMaterials[mesh->mMaterialIndex]);
-
-
-    triModel->header.numberOfVertices      = mesh->mNumFaces*3;
-    triModel->header.numberOfNormals       = mesh->mNumFaces*3;
-    triModel->header.numberOfTextureCoords = mesh->mNumFaces*3;
-    triModel->header.numberOfColors        = mesh->mNumFaces*3;
-    triModel->header.numberOfIndices       = 0;
-
-
-	triModel->vertices       = (float*) malloc( triModel->header.numberOfVertices * 3 * 3  * sizeof(float));
-	triModel->normal         = (float*) malloc( triModel->header.numberOfNormals   * 3 * 3 * sizeof(float));
-	triModel->textureCoords  = (float*) malloc( triModel->header.numberOfTextureCoords    * 2 * 3 * sizeof(float));
-    triModel->colors         = (float*) malloc( triModel->header.numberOfColors * 3 * 3  * sizeof(float));
-    triModel->indices        = 0;
-
-    unsigned int i=0;
-
-    unsigned int o=0,n=0,t=0,c=0;
-	for (i = 0; i < mesh->mNumFaces; i++)
-    {
-		struct aiFace *face = mesh->mFaces + i;
-		unsigned int faceTriA = face->mIndices[0];
-		unsigned int faceTriB = face->mIndices[1];
-		unsigned int faceTriC = face->mIndices[2];
-
-		//fprintf(stderr,"%u / %u \n" , o , triModel->header.numberOfVertices * 3 );
-
-	    triModel->vertices[o++] = mesh->mVertices[faceTriA].x;
-	    triModel->vertices[o++] = mesh->mVertices[faceTriA].y;
-	    triModel->vertices[o++] = mesh->mVertices[faceTriA].z;
-
-	    triModel->vertices[o++] = mesh->mVertices[faceTriB].x;
-	    triModel->vertices[o++] = mesh->mVertices[faceTriB].y;
-	    triModel->vertices[o++] = mesh->mVertices[faceTriB].z;
-
-	    triModel->vertices[o++] = mesh->mVertices[faceTriC].x;
-	    triModel->vertices[o++] = mesh->mVertices[faceTriC].y;
-	    triModel->vertices[o++] = mesh->mVertices[faceTriC].z;
-
-
-      if (mesh->mNormals)
-        {
-			triModel->normal[n++] = mesh->mNormals[faceTriA].x;
-			triModel->normal[n++] = mesh->mNormals[faceTriA].y;
-			triModel->normal[n++] = mesh->mNormals[faceTriA].z;
-
-			triModel->normal[n++] = mesh->mNormals[faceTriB].x;
-			triModel->normal[n++] = mesh->mNormals[faceTriB].y;
-			triModel->normal[n++] = mesh->mNormals[faceTriB].z;
-
-			triModel->normal[n++] = mesh->mNormals[faceTriC].x;
-			triModel->normal[n++] = mesh->mNormals[faceTriC].y;
-			triModel->normal[n++] = mesh->mNormals[faceTriC].z;
-		}
-
-
-      if (mesh->mTextureCoords[0])
-        {
-			triModel->textureCoords[t++] = mesh->mTextureCoords[0][faceTriA].x;
-			triModel->textureCoords[t++] = 1 - mesh->mTextureCoords[0][faceTriA].y;
-
-			triModel->textureCoords[t++] = mesh->mTextureCoords[0][faceTriB].x;
-			triModel->textureCoords[t++] = 1 - mesh->mTextureCoords[0][faceTriB].y;
-
-			triModel->textureCoords[t++] = mesh->mTextureCoords[0][faceTriC].x;
-			triModel->textureCoords[t++] = 1 - mesh->mTextureCoords[0][faceTriC].y;
-		}
-
-
-        unsigned int colourSet = 0;
-        //for (colourSet = 0; colourSet< AI_MAX_NUMBER_OF_COLOR_SETS; colourSet++)
-        {
-          if(mesh->HasVertexColors(colourSet))
-         {
-          triModel->colors[c++] = mesh->mColors[colourSet][faceTriA].r;
-          triModel->colors[c++] = mesh->mColors[colourSet][faceTriA].g;
-          triModel->colors[c++] = mesh->mColors[colourSet][faceTriA].b;
-
-          triModel->colors[c++] = mesh->mColors[colourSet][faceTriB].r;
-          triModel->colors[c++] = mesh->mColors[colourSet][faceTriB].g;
-          triModel->colors[c++] = mesh->mColors[colourSet][faceTriB].b;
-
-          triModel->colors[c++] = mesh->mColors[colourSet][faceTriC].r;
-          triModel->colors[c++] = mesh->mColors[colourSet][faceTriC].g;
-          triModel->colors[c++] = mesh->mColors[colourSet][faceTriC].b;
-        }
-       }
-
-
-
-	}
-
-
-}
-
-
 void prepareMesh(struct aiScene *scene , int meshNumber , struct TRI_Model * triModel )
 {
     struct aiMesh * mesh = scene->mMeshes[meshNumber];
@@ -514,6 +380,20 @@ void prepareScene(struct aiScene *scene , struct TRI_Model * triModel , struct T
 
        fprintf(stderr,"Reading mesh from collada \n");
        prepareMesh(scene, 0,  originalModel );
+
+
+
+ struct skeletonHuman sk={0};
+
+  unsigned int newVal = 200;// rand()%330;
+  sk.relativeJointAngle[HUMAN_SKELETON_LEFT_KNEE].x=newVal;
+  sk.relativeJointAngle[HUMAN_SKELETON_RIGHT_KNEE].x=newVal;
+  sk.relativeJointAngle[HUMAN_SKELETON_LEFT_KNEE].x=newVal;
+  sk.relativeJointAngle[HUMAN_SKELETON_RIGHT_KNEE].x=newVal;
+
+       transMesh( g_scene , 0 , originalModel , &sk );
+
+
        fprintf(stderr,"Flattening mesh\n");
        fillFlatModelTriFromIndexedModelTri(triModel , originalModel);
 
