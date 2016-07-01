7e20b9165357de810694f5bee96f0851faa276e5
Ammar Qammaz <ammarkov@gmail.com>
2016-07-01 17:51:41 +0300

    limits are now ok

--- tools/Primitives/nao_geometry.h
+++ tools/Primitives/nao_geometry.h
@@ -11,20 +11,10 @@
 #define GREEN   "\033[32m"      /* Green */
 #define YELLOW  "\033[33m"      /* Yellow */
 
-
 enum NAO_JOINT_NAMES
 {
-    NullJoint = 0,
-    HeadYaw 	,//Head joint twist (Z) 	-119.5 to 119.5 	-2.0857 to 2.0857
+    HeadYaw  =0	,//Head joint twist (Z) 	-119.5 to 119.5 	-2.0857 to 2.0857
     HeadPitch 	,//Head joint front and back (Y) 	-38.5 to 29.5 	-0.6720 to 0.5149
-
-    LShoulderPitch 	,//Left shoulder joint front and back (Y) 	-119.5 to 119.5 	-2.0857 to 2.0857
-    LShoulderRoll 	,//Left shoulder joint right and left (Z) 	-18 to 76 	-0.3142 to 1.3265
-    LElbowYaw 	,//Left shoulder joint twist (X) 	-119.5 to 119.5 	-2.0857 to 2.0857
-    LElbowRoll 	,//Left elbow joint (Z) 	-88.5 to -2 	-1.5446 to -0.0349
-    LWristYaw 	,//Left wrist joint (X) 	-104.5 to 104.5 	-1.8238 to 1.8238
-    LHand 	,//Right hand 	Open and Close 	Open and Close
-
 
     RShoulderPitch 	,//Right shoulder joint front and back (Y) 	-119.5 to 119.5 	-2.0857 to 2.0857
     RShoulderRoll 	,//Right shoulder joint right and left (Z) 	-76 to 18 	-1.3265 to 0.3142
@@ -33,65 +23,64 @@ enum NAO_JOINT_NAMES
     RWristYaw 	,//Right wrist joint (X) 	-104.5 to 104.5 	-1.8238 to 1.8238
     RHand 	,//Right hand 	Open and Close 	Open and Close
 
+    LShoulderPitch 	,//Left shoulder joint front and back (Y) 	-119.5 to 119.5 	-2.0857 to 2.0857
+    LShoulderRoll 	,//Left shoulder joint right and left (Z) 	-18 to 76 	-0.3142 to 1.3265
+    LElbowYaw 	,//Left shoulder joint twist (X) 	-119.5 to 119.5 	-2.0857 to 2.0857
+    LElbowRoll 	,//Left elbow joint (Z) 	-88.5 to -2 	-1.5446 to -0.0349
+    LWristYaw 	,//Left wrist joint (X) 	-104.5 to 104.5 	-1.8238 to 1.8238
 
-    LHipYawPitch 	,//Left hip joint twist (Y-Z 45°) 	-65.62 to 42.44 	-1.145303 to 0.740810
     RHipYawPitch  	,//Right hip joint twist (Y-Z 45°) 	-65.62 to 42.44 	-1.145303 to 0.740810
-
-
-    LHipRoll 	,//Left hip joint right and left (X) 	-21.74 to 45.29 	-0.379472 to 0.790477
-    LHipPitch 	,//Left hip joint front and back (Y) 	-88.00 to 27.73 	-1.535889 to 0.484090
-    LKneePitch 	,//Left knee joint (Y) 	-5.29 to 121.04 	-0.092346 to 2.112528
-    LAnklePitch ,//	Left ankle joint front and back (Y) 	-68.15 to 52.86 	-1.189516 to 0.922747
-    LAnkleRoll 	,//Left ankle joint right and left (X) 	-22.79 to 44.06 	-0.397880 to 0.769001
-
     RHipRoll 	 ,//Right hip joint right and left (X) 	-45.29 to 21.74 	-0.790477 to 0.379472
     RHipPitch 	 ,//Right hip joint front and back (Y) 	-88.00 to 27.73 	-1.535889 to 0.484090
     RKneePitch 	 ,//Right knee joint (Y) 	-5.90 to 121.47 	-0.103083 to 2.120198
-    RAnklePitch  ,//	Right ankle joint front and back (Y) 	-67.97 to 53.40 	-1.186448 to 0.932056
     RAnkleRoll 	 ,//Right ankle joint right and left (X) 	-44.06 to 22.80 	-0.768992 to 0.397935
+    RAnklePitch  ,//	Right ankle joint front and back (Y) 	-67.97 to 53.40 	-1.186448 to 0.932056
+
+    LHipYawPitch 	,//Left hip joint twist (Y-Z 45°) 	-65.62 to 42.44 	-1.145303 to 0.740810
+    LHipRoll 	,//Left hip joint right and left (X) 	-21.74 to 45.29 	-0.379472 to 0.790477
+    LHipPitch 	,//Left hip joint front and back (Y) 	-88.00 to 27.73 	-1.535889 to 0.484090
+    LKneePitch 	,//Left knee joint (Y) 	-5.29 to 121.04 	-0.092346 to 2.112528
+    LAnkleRoll 	,//Left ankle joint right and left (X) 	-22.79 to 44.06 	-0.397880 to 0.769001
+    LAnklePitch ,//	Left ankle joint front and back (Y) 	-68.15 to 52.86 	-1.189516 to 0.922747
+
    //--------------------------------------------
     NUMBER_OF_NAO_JOINTS
 };
 
 
-static const char * NAOjointNames[] =
+static const char * naoJointNames[] =
 {
-    "NullJoint" ,
-    "HeadYaw" 	,
-    "HeadPitch" ,
+    "HeadYaw"   	,
+    "HeadPitch" 	,
 
-    "LShoulderPitch",
-    "LShoulderRoll" ,
-    "LElbowYaw" 	,
-    "LElbowRoll" 	,
-    "LWristYaw" 	,
-    "LHand" 	    ,
-
-    "RShoulderPitch",
-    "RShoulderRoll" ,
+    "RShoulderPitch" 	,
+    "RShoulderRoll" 	,
     "RElbowYaw" 	,
     "RElbowRoll" 	,
     "RWristYaw" 	,
-    "RHand" 	    ,
+    "RHand" 	,
 
+    "LShoulderPitch" 	,
+    "LShoulderRoll" 	,
+    "LElbowYaw" 	,
+    "LElbowRoll" 	,
+    "LWristYaw" 	,
+
+    "RHipYawPitch"  	,
+    "RHipRoll" 	     ,
+    "RHipPitch" 	 ,
+    "RKneePitch" 	 ,
+    "RAnkleRoll" 	 ,
+    "RAnklePitch"  ,
 
     "LHipYawPitch" 	,
-    "RHipYawPitch"  ,
-
-
-    "LHipRoll" 	 ,
-    "LHipPitch"  ,
-    "LKneePitch" ,
-    "LAnklePitch",
-    "LAnkleRoll" ,
-
-    "RHipRoll"	 ,
-    "RHipPitch"  ,
-    "RKneePitch" ,
-    "RAnklePitch",
-    "RAnkleRoll" ,
- //=================
- "End of Joint Names"
+    "LHipRoll" 	    ,
+    "LHipPitch" 	,
+    "LKneePitch" 	,
+    "LAnkleRoll" 	,
+    "LAnklePitch"   ,
+   //--------------------------------------------
+    "NUMBER_OF_NAO_JOINTS"
 };
 
 
@@ -99,36 +88,38 @@ static const char * NAOjointNames[] =
 
 
 static float NAO_min[] =
-{    0,
+{
      -119.5  ,
      -38.5   ,
-     -119.5  ,
-     -18     ,
-     -119.5  ,
-     -88.5   ,
-     -104.5  ,
-     -1      , // Left Hand
+
      -119.5  ,
      -76     ,
      -119.5  ,
      2       ,
      -104.5  ,
-     -1 	 , // Right hand
+
+     -119.5  ,
+     -18     ,
+     -119.5  ,
+     -88.5   ,
+     -104.5  ,
 
      -65.62 ,
-     -65.62 ,
-
-     -21.74 ,
-     -88.00 ,
-     -5.29 ,
-     -68.15  ,
-     -22.79  ,
-
      -45.29  ,
      -88.00  ,
      -5.90  ,
+     -44.06  ,
      -67.97  ,
-     -44.06
+
+     -65.62 ,
+     -21.74 ,
+     -88.00 ,
+     -5.29 ,
+     -22.79  ,
+     -68.15  ,
+
+     -1 	 , // Right hand
+     -1       // Left Hand
 };
 
 
@@ -137,16 +128,9 @@ static float NAO_min[] =
 
 
 static float NAO_max[] =
-{   0,
+{
     119.5 ,
     29.5  ,
-
-    119.5 ,
-    76    ,
-    119.5 ,
-    -2    ,
-    104.5 ,
-    1.0	,//Right hand 	Open and Close 	Open and Close
 
 
     119.5 	,
@@ -154,24 +138,31 @@ static float NAO_max[] =
     119.5 	,
     88.5 	,
     104.5 	,
-    1.0 	,//Right hand 	Open and Close 	Open and Close
+
+
+    119.5 ,
+    76    ,
+    119.5 ,
+    -2    ,
+    104.5 ,
 
 
     42.44 	,
-    42.44 	,
-
-
-    45.29 	,
-    27.73 	,
-    121.04 	,
-    52.86 	,
-    44.06 	,
-
     21.74 	,
     27.73 	,
     121.47 	,
+    22.80   ,
     53.40 	,
-    22.80
+
+    42.44 	,
+    45.29 	,
+    27.73 	,
+    121.04 	,
+    44.06 	,
+    52.86 	,
+
+    1.0	    ,//Right hand 	Open and Close 	Open and Close
+    1.0 	 //Left hand 	Open and Close 	Open and Close
     };
 
 
