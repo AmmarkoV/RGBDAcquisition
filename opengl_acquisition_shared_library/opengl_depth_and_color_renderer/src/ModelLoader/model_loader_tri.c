f28ed745233b0ad942f516ec1a96a1ee1ee4e7f4
Ammar Qammaz <ammarkov@gmail.com>
2016-07-29 16:55:37 +0300

    tri drawer now can also draw indexed meshes

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/ModelLoader/model_loader_tri.c
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/ModelLoader/model_loader_tri.c
@@ -36,7 +36,7 @@ int fillFlatModelTriFromIndexedModelTri(struct TRI_Model * triModel , struct TRI
     {
 		unsigned int faceTriA = indexed->indices[(i*3)+0];
 		unsigned int faceTriB = indexed->indices[(i*3)+1];
-		unsigned int faceTriC = indexed->indices[(i*3)+02];
+		unsigned int faceTriC = indexed->indices[(i*3)+2];
 
 
         unsigned int faceTriA_X = (faceTriA*3)+0;
@@ -354,31 +354,61 @@ int saveModelTriHeader(const char * filename , struct TRI_Model * triModel)
 
 
 
-
+//#define INCLUDE_OPENGL_CODE 1
 void doTriDrawCalllist(struct TRI_Model * tri )
 {
  #if INCLUDE_OPENGL_CODE
-  glBegin(GL_TRIANGLES);
+  unsigned int i=0,z=0;
 
- unsigned int i=0,z=0;
+  glBegin(GL_TRIANGLES);
+    if (tri->header.numberOfIndices > 0 )
+    {
+     unsigned int faceTriA,faceTriB,faceTriC,faceTriA_X,faceTriA_Y,faceTriA_Z,faceTriB_X,faceTriB_Y,faceTriB_Z,faceTriC_X,faceTriC_Y,faceTriC_Z;
+
+     for (i = 0; i < tri->header.numberOfIndices/3; i++)
+     {
+      faceTriA = tri->indices[(i*3)+0];      faceTriB = tri->indices[(i*3)+1];      faceTriC = tri->indices[(i*3)+2];
+      faceTriA_X = (faceTriA*3)+0;           faceTriA_Y = (faceTriA*3)+1;           faceTriA_Z = (faceTriA*3)+2;
+      faceTriB_X = (faceTriB*3)+0;           faceTriB_Y = (faceTriB*3)+1;           faceTriB_Z = (faceTriB*3)+2;
+      faceTriC_X = (faceTriC*3)+0;           faceTriC_Y = (faceTriC*3)+1;           faceTriC_Z = (faceTriC*3)+2;
+
+      if (tri->normal)
+        { glNormal3f(tri->normal[faceTriA_X],tri->normal[faceTriA_Y],tri->normal[faceTriA_Z]); }
+      if ( tri->colors )
+        { glColor3f(tri->colors[faceTriA_X],tri->colors[faceTriA_Y],tri->colors[faceTriA_Z]);  }
+      glVertex3f(tri->vertices[faceTriA_X],tri->vertices[faceTriA_Y],tri->vertices[faceTriA_Z]);
+
+      if (tri->normal)
+        { glNormal3f(tri->normal[faceTriB_X],tri->normal[faceTriB_Y],tri->normal[faceTriB_Z]); }
+      if ( tri->colors )
+        { glColor3f(tri->colors[faceTriB_X],tri->colors[faceTriB_Y],tri->colors[faceTriB_Z]);  }
+      glVertex3f(tri->vertices[faceTriB_X],tri->vertices[faceTriB_Y],tri->vertices[faceTriB_Z]);
+
+      if (tri->normal)
+        { glNormal3f(tri->normal[faceTriC_X],tri->normal[faceTriC_Y],tri->normal[faceTriC_Z]); }
+      if ( tri->colors )
+        { glColor3f(tri->colors[faceTriC_X],tri->colors[faceTriC_Y],tri->colors[faceTriC_Z]);  }
+      glVertex3f(tri->vertices[faceTriC_X],tri->vertices[faceTriC_Y],tri->vertices[faceTriC_Z]);
+	 }
+    } else
+    {
       for (i=0; i<tri->header.numberOfVertices/3; i++)
         {
          z=(i*3)*3;
-                      glNormal3f(tri->normal[z+0],tri->normal[z+1],tri->normal[z+2]);
-                      glColor3f(tri->colors[z+0],tri->colors[z+1],tri->colors[z+2]);
-                      glVertex3f(tri->vertices[z+0],tri->vertices[z+1],tri->vertices[z+2]);
+         if (tri->normal) { glNormal3f(tri->normal[z+0],tri->normal[z+1],tri->normal[z+2]); }
+         if (tri->colors) { glColor3f(tri->colors[z+0],tri->colors[z+1],tri->colors[z+2]);  }
+                            glVertex3f(tri->vertices[z+0],tri->vertices[z+1],tri->vertices[z+2]);
 
          z+=3;
-                      glNormal3f(tri->normal[z+0],tri->normal[z+1],tri->normal[z+2]);
-                      glColor3f(tri->colors[z+0],tri->colors[z+1],tri->colors[z+2]);
-                      glVertex3f(tri->vertices[z+0],tri->vertices[z+1],tri->vertices[z+2]);
+         if (tri->normal) { glNormal3f(tri->normal[z+0],tri->normal[z+1],tri->normal[z+2]); }
+         if (tri->colors) { glColor3f(tri->colors[z+0],tri->colors[z+1],tri->colors[z+2]);  }
+                            glVertex3f(tri->vertices[z+0],tri->vertices[z+1],tri->vertices[z+2]);
          z+=3;
-                      glNormal3f(tri->normal[z+0],tri->normal[z+1],tri->normal[z+2]);
-                      glColor3f(tri->colors[z+0],tri->colors[z+1],tri->colors[z+2]);
-                      glVertex3f(tri->vertices[z+0],tri->vertices[z+1],tri->vertices[z+2]);
+         if (tri->normal) { glNormal3f(tri->normal[z+0],tri->normal[z+1],tri->normal[z+2]); }
+         if (tri->colors) { glColor3f(tri->colors[z+0],tri->colors[z+1],tri->colors[z+2]);  }
+                            glVertex3f(tri->vertices[z+0],tri->vertices[z+1],tri->vertices[z+2]);
         }
-
-
+    }
   glEnd();
  #else
   fprintf(stderr,"OpenGL code not compiled in this model loader TRI code.. \n");
