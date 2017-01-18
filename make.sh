83bcbcc1623ea3a1be8231f3342adb30533e2137
Ammar Qammaz <ammarkov@gmail.com>
2017-01-18 03:53:16 +0200

    ...

--- make.sh
+++ make.sh
@@ -9,6 +9,12 @@ cd build
 cmake ..
 
 make
+notify-send "Done Compiling RGBDAcquisition"
+
+
+cd /home/ammar/Documents/Programming/FORTH/input_acquisition/opengl_acquisition_shared_library/opengl_depth_and_color_renderer/build
+make 
+notify-send "Done Compiling OpenGLRenderer"
 
 cd ..
 
