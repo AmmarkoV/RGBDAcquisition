f7d1bef89024f144e0dbb9050c45982cacf7c6ce
Ammar Qammaz <ammarkov@gmail.com>
2017-02-10 00:44:34 +0200

    adding a blob detector ..

---
+++ processors/BlobDetector/BlobDetector.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "BlobDetector.h"
+
+
+
+
+int initArgs_BlobDetector(int argc, char *argv[])
+{
+
+}
+
+int setConfigStr_BlobDetector(char * label,char * value)
+{
+
+}
+
+int setConfigInt_BlobDetector(char * label,int value)
+{
+
+}
+
+
+
+unsigned char * getDataOutput_BlobDetector(unsigned int stream , unsigned int * width, unsigned int * height,unsigned int * channels,unsigned int * bitsperpixel)
+{
+
+}
+
+int addDataInput_BlobDetector(unsigned int stream , void * data, unsigned int width, unsigned int height,unsigned int channels,unsigned int bitsperpixel)
+{
+
+}
+
+
+
+unsigned short * getDepth_BlobDetector(unsigned int * width, unsigned int * height,unsigned int * channels,unsigned int * bitsperpixel)
+{
+
+}
+
+
+unsigned char * getColor_BlobDetector(unsigned int * width, unsigned int * height,unsigned int * channels,unsigned int * bitsperpixel)
+{
+
+}
+
+
+
+int processData_BlobDetector()
+{
+
+}
+
+
+int cleanup_BlobDetector()
+{
+
+}
+
+
+int stop_BlobDetector()
+{
+
+}
+
---
+++ processors/BlobDetector/BlobDetector.h
@@ -0,0 +1,35 @@
+#ifndef BLOBDETECTOR_H_INCLUDED
+#define BLOBDETECTOR_H_INCLUDED
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+int initArgs_BlobDetector(int argc, char *argv[]);
+
+int setConfigStr_BlobDetector(char * label,char * value);
+int setConfigInt_BlobDetector(char * label,int value);
+
+
+unsigned char * getDataOutput_BlobDetector(unsigned int stream , unsigned int * width, unsigned int * height,unsigned int * channels,unsigned int * bitsperpixel);
+int addDataInput_BlobDetector(unsigned int stream , void * data, unsigned int width, unsigned int height,unsigned int channels,unsigned int bitsperpixel);
+
+
+unsigned short * getDepth_BlobDetector(unsigned int * width, unsigned int * height,unsigned int * channels,unsigned int * bitsperpixel);
+unsigned char * getColor_BlobDetector(unsigned int * width, unsigned int * height,unsigned int * channels,unsigned int * bitsperpixel);
+
+
+int processData_BlobDetector();
+
+int cleanup_BlobDetector();
+
+int stop_BlobDetector();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BLOBDETECTOR_H_INCLUDED
