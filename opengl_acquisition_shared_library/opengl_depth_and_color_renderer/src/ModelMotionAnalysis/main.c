f6ecdd0c8ad0dd206f6d3a0bcb98cb28efeaf4ae
Ammar Qammaz <ammarkov@ics.forth.gr>
2017-03-21 17:22:05 +0200

    making a small parser to extract motion analysis..!

---
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/ModelMotionAnalysis/main.c
@@ -0,0 +1,18 @@
+/** @file main.c
+ *  @brief  A minimal binary that renders scene files using OGLRendererSandbox s
+ *          X86 compilation: gcc -o -L/usr/X11/lib   main main.c -lGL -lX11 -lpng -ljpeg
+ *          X64 compilation: gcc -o -L/usr/X11/lib64 main main.c -lGL -lX11 -lpng -ljpeg
+ *  @author Ammar Qammaz (AmmarkoV)
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv)
+{
+
+
+    return 0;
+}
