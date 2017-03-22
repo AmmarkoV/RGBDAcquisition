c8032fe76e8c4db2f83d6619d5f93c0740e91ae8
Ammar Qammaz <ammarkov@gmail.com>
2017-03-22 16:34:01 +0200

    adding a script to package ROS Node..

---
+++ 3dparty/ROS/packageROSNode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+red=$(printf "\033[31m")
+green=$(printf "\033[32m")
+yellow=$(printf "\033[33m")
+blue=$(printf "\033[34m")
+magenta=$(printf "\033[35m")
+cyan=$(printf "\033[36m")
+white=$(printf "\033[37m")
+normal=$(printf "\033[m")
+
+
+THEDATETAG=`date +"%y-%m-%d_%H-%M-%S"`
+
+OURDISTRO=`lsb_release -a | grep "Distributor ID" | cut  -f2`
+OURVER=`lsb_release -a | grep Release | cut  -f2`
+echo "Our Distro is $OURDISTRO $OURVER"
+
+
+NAME="hobbitSignTrackerTest$OURDISTRO$OURVER-$THEDATETAG"
+ 
+
+tar cvfjh "$NAME.tar.bz2" rgbd_acquisition/
+ 
+
+echo "$green Done.. $normal"
+exit 0
