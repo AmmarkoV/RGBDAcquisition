9b7b60535706e59d79fe963249e9b4e2bff3bf04
Ammar Qammaz <ammarkov@ics.forth.gr>
2017-05-15 11:41:13 +0300

    ...

--- grabber/frames/runRTInAllSubdirectoriesAndGenerateCSV.sh
+++ grabber/frames/runRTInAllSubdirectoriesAndGenerateCSV.sh
@@ -5,11 +5,11 @@ cd "$DIR"
 
 
 for f in $(find $1 -depth -type d); do  
-       echo "Dir $f" 
-      # ./runRTPose.sh $f 
+       echo "Running Neural Network on Dataset.. $f" 
+       ./runRTPose.sh $f 
 done
  
-echo "Compressing output"
+echo "Compressing All Output.."
 tar -czvf csvFiles.tar.gz `find  $1 -depth | grep '.csv'`
 
 
