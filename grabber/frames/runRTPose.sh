7fa8a315fd521bb91bd6e340cb10d977e249203b
Ammar Qammaz <ammarkov@ics.forth.gr>
2017-05-15 11:42:23 +0300

    ...

--- grabber/frames/runRTPose.sh
+++ grabber/frames/runRTPose.sh
@@ -8,7 +8,7 @@ CAFFE_DIR="/home/ammar/Documents/3dParty/Realtime_Multi-Person_Pose_Estimation/c
 cd $CAFFE_DIR
 echo "Will try to output results to $DIR/$1/dnnOut" 
 
-#./build/examples/rtpose/rtpose.bin --image_dir $DIR/$1/ --no_frame_drops --write_json $DIR/$1/dnnOut --logtostderr 
+./build/examples/rtpose/rtpose.bin --image_dir $DIR/$1/ --no_frame_drops --write_json $DIR/$1/dnnOut --logtostderr 
  
 rm $DIR/$1/dnnOut/depth*
 rm $DIR/$1/dnnOut/converted.csv
