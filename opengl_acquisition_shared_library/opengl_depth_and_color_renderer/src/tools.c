52fbed2506cc761ca8b58ab4ffd9ad35cf3c7072
Ammar Qammaz <ammarkov@ics.forth.gr>
2017-07-11 18:59:14 +0300

    ...

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/tools.c
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/tools.c
@@ -26,9 +26,9 @@ void printOpenGLError(int errorCode)
     case  GL_INVALID_OPERATION :
          fprintf(stderr,"The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag.");
         break;
-    case  GL_INVALID_FRAMEBUFFER_OPERATION :
-         fprintf(stderr,"The framebuffer object is not complete. The offending command is ignored and has no other side effect than to set the error flag.");
-        break;
+    //case  GL_INVALID_FRAMEBUFFER_OPERATION :
+    //     fprintf(stderr,"The framebuffer object is not complete. The offending command is ignored and has no other side effect than to set the error flag.");
+    //    break;
     case  GL_OUT_OF_MEMORY :
          fprintf(stderr,"There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded.");
         break;
