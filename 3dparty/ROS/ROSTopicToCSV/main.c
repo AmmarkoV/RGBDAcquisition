cdca914084f3f061ccdd24dc9a7bb278142c57a8
Ammar Qammaz <ammarkov@ics.forth.gr>
2017-10-03 15:02:04 +0300

    rostopic to csv now working..

--- 3dparty/ROS/ROSTopicToCSV/main.c
+++ 3dparty/ROS/ROSTopicToCSV/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../../../tools/Primitives/CompareBody/InputParser_C.h"
 
+
+int removeLineEndings(char * str)
+{
+  int end=strlen(str);
+  if (end>0) { if ( (str[end]==10) || (str[end]==13) )  { str[end]=0; } }
+  if (end>1) { if ( (str[end-1]==10) || (str[end-1]==13) )  { str[end-1]=0; } }
+  if (end>2) { if ( (str[end-2]==10) || (str[end-2]==13) )  { str[end-2]=0; } }
+ return 1;
+}
 
 int parseFile(const char * filename)
 {
- printf("Parsing rostopic %s !\n",filename);
+ //printf("Parsing rostopic %s !\n",filename);
  //fprintf(stderr,"Running COCO 2D skeleton (%s)\n",filename);
 
 //  char * line = NULL;
@@ -16,28 +24,35 @@ int parseFile(const char * filename)
   FILE * fp = fopen(filename,"r");
   if (fp!=0)
   {
-   struct InputParserC * ipc = InputParser_Create(1024,4);
-
-   InputParser_SetDelimeter(ipc,0,':');
-   InputParser_SetDelimeter(ipc,1,'[');
-   InputParser_SetDelimeter(ipc,2,',');
-   InputParser_SetDelimeter(ipc,3,']');
-
-    char type[512];
-    char subtype[512];
-    char content[512];
-    char str[512];
+    char csvHeader[4096]={0};
+    char csvLastContent[4096]={0};
+    int csvContentFields=0;
+    int csvHeaderFields=0;
+    int csvHeaderEmmited=0;
+    char type[512]={0};
+    char subtype[512]={0};
+    char content[512]={0};
+    char str[512]={0};
     char * line = NULL;
     size_t len = 0;
 
 
     while ((read = getline(&line, &len, fp)) != -1)
     {
-       //printf("Retrieved line of length %zu :\n", read);
-      printf("%s", line);
+      //fprintf(stderr,"Retrieved line of length %zu :\n", read);
+      //fprintf(stderr,"%s", line);
       if (strstr(line,"---")!=0)
       {
-       printf("NEXT MESSAGE , do flush here\n");
+       if (!csvHeaderEmmited)
+       {
+        fprintf(stdout,"%s\n",csvHeader);
+        csvHeaderEmmited=1;
+       }
+        fprintf(stdout,"%s\n",csvLastContent);
+
+       type[0]=0; subtype[0]=0; content[0]=0;
+       csvContentFields=0;
+       //fprintf(stderr,"NEXT MESSAGE , do flush here\n");
       } else
       {
       char * foundColonNL=strstr(line,": \n");
@@ -45,7 +60,7 @@ int parseFile(const char * filename)
       {
        *foundColonNL=0;
        snprintf(type,512,"%s",line);
-       printf("PREVIOUSLINETYPE(%s)\n",type);
+       //fprintf(stderr,"PREVIOUSLINETYPE(%s)\n",type);
       } else
       {
         char * foundContentAfterColon=strstr(line,": ");
@@ -55,24 +70,29 @@ int parseFile(const char * filename)
              (foundContentAfterColon!=0)
             )
         {
+
            *foundContentAfterColon=0;
            snprintf(subtype,512,"%s",line);
            snprintf(content,512,"%s",foundContentAfterColon+2);
-           printf("SUBTYPE(%s)\n",subtype+2);
-           printf("CONTENT(%s)\n",content);
+           //fprintf(stderr,"SUBTYPE(%s)\n",subtype+2);
+           removeLineEndings(content);
+           //fprintf(stderr,"CONTENT(%s)\n",content);
+
+
+           char tmp[4096]={0};
+           if (csvHeaderFields>0) { snprintf(tmp,4096,"%s,%s_%s",csvHeader,type,subtype+2); } else
+                                  { snprintf(tmp,4096,"%s_%s",type,subtype+2); }
+           snprintf(csvHeader,4096,"%s",tmp);
+
+
+           if (csvContentFields>0) { snprintf(tmp,4096,"%s,%s",csvLastContent,content); } else
+                                   { snprintf(tmp,4096,"%s",content); }
+           snprintf(csvLastContent,4096,"%s",tmp);
+
+
+           ++csvContentFields;
+           ++csvHeaderFields;
         }
-
-           if (strstr(line,"joints")!=0)
-      {
-       printf("%s", line);
-       int i=0;
-       int num = InputParser_SeperateWords(ipc,line,1);
-
-
-
-
-      ++frameNumber;
-       }
       }
       }
       }
