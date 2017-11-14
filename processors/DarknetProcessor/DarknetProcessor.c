03a0e05b281c68cf82237a22553fdfc65b57f7ae
Ammar Qammaz <ammarkov@gmail.com>
2017-11-15 01:26:28 +0200

    darknet working now..

--- processors/DarknetProcessor/DarknetProcessor.c
+++ processors/DarknetProcessor/DarknetProcessor.c
@@ -18,15 +18,17 @@ struct darknetContext
  float nms;
  box *boxes;
  float **probs;
+ float **masks;
+ char **names;// = get_labels(name_list);
+
+ float *avg;
 
  float threshold;
+ float hierarchyThreshold;
 };
 
 
-struct darknetContext dc;
-
-char *voc_names[] = {"aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"};
-
+struct darknetContext dc={0};
 
 
 image load_image_from_buffer(void * pixels , unsigned int width, unsigned int height, unsigned int channels)
@@ -58,31 +60,47 @@ image load_image_from_buffer(void * pixels , unsigned int width, unsigned int he
 int init_yolo(
                 const char *cfgfile,
                 const char *weightfile,
+                const char *datafile,
                 float thresh
                )
 {
     if (!cfgfile)    { return 0; }
     if (!weightfile) { return 0; }
+    if (!datafile) { return 0; }
 
     dc.alphabet = load_alphabet();
-    dc.net= parse_network_cfg(cfgfile);
-    if(weightfile){
-                   load_weights(dc.net, weightfile);
-                  }
-    dc.l = dc.net->layers[dc.net->n-1];
 
+
+    list *options = read_data_cfg(datafile);
+    int classes = option_find_int(options, "classes", 20);
+    char *name_list = option_find_str(options, "names", "data/names.list");
+    dc.names = get_labels(name_list);
+
+    dc.net = load_network(cfgfile, weightfile, 0);
     set_batch_network(dc.net, 1);
+
+
     srand(2222222);
 
-
+    dc.l = dc.net->layers[dc.net->n-1];
     dc.nms=.4;
-    dc.boxes = calloc(dc.l.side * dc.l.side * dc.l.n, sizeof(box));
-    dc.probs = calloc(dc.l.side * dc.l.side * dc.l.n, sizeof(float *));
+    dc.threshold=thresh;
+    dc.hierarchyThreshold=0.5;
+
+    dc.avg = (float *)   calloc(dc.l.outputs, sizeof(float));
+
+    dc.boxes = (box *)   calloc( dc.l.w * dc.l.h * dc.l.n, sizeof(box));
+    dc.probs = (float **)calloc( dc.l.w * dc.l.h * dc.l.n, sizeof(float *));
 
     int j;
-    for(j = 0; j < dc.l.side*dc.l.side*dc.l.n; ++j)
-         { dc.probs[j] = calloc(dc.l.classes, sizeof(float *)); }
+    for(j = 0; j < dc.l.w * dc.l.h * dc.l.n; ++j)
+         { dc.probs[j] = (float *)calloc(dc.l.classes+1, sizeof(float)); }
 
+     if (dc.l.coords > 4)
+        {
+            dc.masks = calloc(dc.l.w*dc.l.h*dc.l.n, sizeof(float*));
+            for(j = 0; j < dc.l.w*dc.l.h*dc.l.n; ++j) dc.masks[j] = calloc(dc.l.coords-4, sizeof(float *));
+        }
 
  return 1;
 }
@@ -91,7 +109,8 @@ int initArgs_DarknetProcessor(int argc, char *argv[])
 {
  char * cfgFile=0;
  char * weightFile=0;
- float threshold=0.2;
+ char * dataFile=0;
+ float threshold=0.25;
 
 
  unsigned int i=0;
@@ -99,11 +118,13 @@ int initArgs_DarknetProcessor(int argc, char *argv[])
  {
    if (strstr(argv[i],".cfg")!=0) { cfgFile=argv[i]; }
    if (strstr(argv[i],".weights")!=0) { weightFile=argv[i]; }
+   if (strstr(argv[i],".data")!=0) { dataFile=argv[i]; }
  }
 
  return init_yolo(
                    cfgFile,
                    weightFile,
+                   dataFile,
                    threshold
                  );
 }
@@ -115,26 +136,46 @@ int addDataInput_DarknetProcessor(unsigned int stream , void * data, unsigned in
  fprintf(stderr,"addDataInput_DarknetProcessor %u (%ux%u) channels=%u\n" , stream , width, height,channels);
  if (stream==0)
  {
-    fprintf(stderr,"casting image..\n");
     image im=load_image_from_buffer(data, width, height, channels);
-    show_image(im, "original");
-    //save_image(im, "original");
-    fprintf(stderr,"resizing image..\n");
-    image sized = resize_image(im, dc.net->w, dc.net->h);
-    float *X = sized.data;
+    //image sized = resize_image(im, dc.net->w, dc.net->h);
+    image sized = letterbox_image(im, dc.net->w, dc.net->h);
+
+
+    layer l = dc.net->layers[dc.net->n-1];
 
     fprintf(stderr,"detecting.. ");
-    float *prediction = network_predict(dc.net, X);
+    float *prediction = network_predict(dc.net /*Neural Net*/, sized.data /*Search Image*/);
+    fprintf(stderr,"done ( %u )\n",dc.l.outputs);
+
+
+    float * predictionCurrent = (float *) calloc(l.outputs, sizeof(float));
+
+
+    unsigned int detections =  l.w * l.h * l.n;
+
+
+    if(l.type == DETECTION)   {
+                               fprintf(stderr,"getting_detection_boxes .. ");
+                               get_detection_boxes(l, 1, 1, dc.threshold, dc.probs, dc.boxes, 0);
+                              }
+                              else
+    if (l.type == REGION)  {
+                               fprintf(stderr,"getting_region_boxes .. ");
+                               get_region_boxes(l, im.w, im.h, dc.net->w, dc.net->h, dc.threshold, dc.probs, dc.boxes, 0, 0, 0, dc.hierarchyThreshold, 1);
+                              } else
+                              {
+                               error("Last layer must produce detections\n");
+                              }
     fprintf(stderr,"done\n");
 
-    fprintf(stderr,"getting results ( %u outputs , %u boxes ) ..\n",dc.l.outputs );
-    get_detection_boxes(dc.l, 1, 1, dc.threshold, dc.probs, dc.boxes, 0);
+
 
     if (dc.nms)
-         { do_nms_sort(dc.boxes, dc.probs, dc.l.side* dc.l.side* dc.l.n, dc.l.classes, dc.nms); }
+         { do_nms_sort(dc.boxes, dc.probs, l.w*l.h*l.n, l.classes, dc.nms); }
 
-    printf("Objects:\n\n");
-    draw_detections(im, dc.l.side * dc.l.side * dc.l.n  , dc.threshold , dc.boxes, dc.probs, 0, voc_names, dc.alphabet, 20/*classes*/);
+    printf("Objects (%u classes):\n\n",l.classes);
+
+    draw_detections(im, detections , dc.threshold, dc.boxes, dc.probs, dc.masks, dc.names , dc.alphabet, l.classes);
     save_image(im, "predictions");
     show_image(im, "predictions");
 
@@ -172,14 +213,11 @@ unsigned short * getDepth_DarknetProcessor(unsigned int * width, unsigned int * 
 
 }
 
-
 unsigned char * getColor_DarknetProcessor(unsigned int * width, unsigned int * height,unsigned int * channels,unsigned int * bitsperpixel)
 {
  return 0;
 
 }
-
-
 
 int processData_DarknetProcessor()
 {
