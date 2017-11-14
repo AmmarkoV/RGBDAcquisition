80e8bbd59efc9923d361694c38f3021673c4c579
Ammar Qammaz <ammarkov@gmail.com>
2017-11-14 03:01:32 +0200

    ...

--- processors/DarknetProcessor/DarknetProcessor.c
+++ processors/DarknetProcessor/DarknetProcessor.c
@@ -32,11 +32,15 @@ char *voc_names[] = {"aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "c
 image load_image_from_buffer(void * pixels , unsigned int width, unsigned int height, unsigned int channels)
 {
     char * data = (char*) pixels;
-    int w=width, h=height, c=channels;
+    int w=width, h=height, c=channels , step=width*channels;
     int i,j,k;
     image im = make_image(w, h, c);
-    for(k = 0; k < c; ++k){
-        for(j = 0; j < h; ++j){
+
+
+    for(k = 0; k < c; ++k)
+    {
+        for(j = 0; j < h; ++j)
+        {
             for(i = 0; i < w; ++i){
                 int dst_index = i + w*j + w*h*k;
                 int src_index = k + c*i + c*w*j;
@@ -44,6 +48,7 @@ image load_image_from_buffer(void * pixels , unsigned int width, unsigned int he
             }
         }
     }
+
     //free(data);
     return im;
 }
@@ -86,7 +91,7 @@ int initArgs_DarknetProcessor(int argc, char *argv[])
 {
  char * cfgFile=0;
  char * weightFile=0;
- float threshold=0.4;
+ float threshold=0.2;
 
 
  unsigned int i=0;
@@ -96,13 +101,11 @@ int initArgs_DarknetProcessor(int argc, char *argv[])
    if (strstr(argv[i],".weights")!=0) { weightFile=argv[i]; }
  }
 
- init_yolo(
-           cfgFile,
-           weightFile,
-           threshold
-           );
-
- return 0;
+ return init_yolo(
+                   cfgFile,
+                   weightFile,
+                   threshold
+                 );
 }
 
 int setConfigStr_DarknetProcessor(char * label,char * value)
@@ -130,13 +133,17 @@ int addDataInput_DarknetProcessor(unsigned int stream , void * data, unsigned in
  fprintf(stderr,"addDataInput_DarknetProcessor %u (%ux%u) channels=%u\n" , stream , width, height,channels);
  if (stream==0)
  {
+    fprintf(stderr,"casting image..\n");
     image im=load_image_from_buffer(data, width, height, channels);
+    show_image(im, "original");
+    save_image(im, "original");
     fprintf(stderr,"resizing image..\n");
     image sized = resize_image(im, dc.net.w, dc.net.h);
     float *X = sized.data;
 
-    fprintf(stderr,"detecting..\n");
+    fprintf(stderr,"detecting.. ");
     network_predict(dc.net, X);
+    fprintf(stderr,"done\n");
 
     fprintf(stderr,"getting results..\n");
     get_detection_boxes(dc.l, 1, 1, dc.threshold, dc.probs, dc.boxes, 0);
@@ -144,12 +151,13 @@ int addDataInput_DarknetProcessor(unsigned int stream , void * data, unsigned in
     if (dc.nms)
          { do_nms_sort(dc.boxes, dc.probs, dc.l.side* dc.l.side* dc.l.n, dc.l.classes, dc.nms); }
 
-    draw_detections(im, dc.l.side * dc.l.side * dc.l.n  , dc.threshold , dc.boxes, dc.probs, 0, voc_names, dc.alphabet, 20);
-        save_image(im, "predictions");
-        show_image(im, "predictions");
+    printf("Objects:\n\n");
+    draw_detections(im, dc.l.side * dc.l.side * dc.l.n  , dc.threshold , dc.boxes, dc.probs, 0, voc_names, dc.alphabet, 20/*classes*/);
+    save_image(im, "predictions");
+    show_image(im, "predictions");
 
-        free_image(im);
-        free_image(sized);
+    free_image(im);
+    free_image(sized);
 
    ++framesProcessed;
   return 1;
