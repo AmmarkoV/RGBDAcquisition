46781a25d3b3874b2105f5eab884b048818124c7
Ammar Qammaz <ammarkov@gmail.com>
2017-12-06 17:47:12 +0200

    darknet cmake file now uses static linking..

--- run_viewer.sh
+++ run_viewer.sh
@@ -12,7 +12,7 @@ if [ -e $BIN ]
 then
  ldd $BIN | grep not
 #LD_LIBRARY_PATH=.:../3dparty/libfreenect/build/lib:$LD_LIBRARY_PATH 
- ./$BIN $@
+ LD_LIBRARY_PATH=. ./$BIN $@
 else
  if [ -e $ORIGINALDIRBIN ]
    then 
