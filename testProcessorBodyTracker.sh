0a363052b70d17d025df482a8f21b7c2145511d1
Ammar Qammaz <ammarkov@gmail.com>
2018-05-04 12:12:55 +0300

    ...

--- testProcessorBodyTracker.sh
+++ testProcessorBodyTracker.sh
@@ -7,6 +7,10 @@ cd redist
 LIBS="../processors/BodyTracker/forth_skeleton_tracker_redist"
 
 #LD_PRELOAD=$LIBS/libboost_system.so.1.57.0:$LIBS/libAcquisition.so:$LIBS/libCore.so:$LIBS/../libBodyTracker.so 
-LD_LIBRARY_PATH=/home/ammar/Documents/Programming/RGBDAcquisition/processors/BodyTracker/forth_skeleton_tracker_redist/:/home/ammar/Downloads/opencvlib:.  ./Viewer -module OPENNI2 -from 0 -processor /home/ammar/Documents/Programming/RGBDAcquisition/processors/BodyTracker/libBodyTracker.so  BodyTracker $@
-  
+#LD_LIBRARY_PATH=/home/ammar/Documents/Programming/RGBDAcquisition/processors/BodyTracker/forth_skeleton_tracker_redist/:/home/ammar/Downloads/opencvlib:.  ./Viewer -module OPENNI2 -from 0 -processor /home/ammar/Documents/Programming/RGBDAcquisition/processors/BodyTracker/libBodyTracker.so  BodyTracker $@
+ 
+
+cd /home/ammar/Documents/Programming/FORTH/input_acquisition/processors/BodyTracker/forth_skeleton_tracker_redist
+./testProcessorBodyTracker.sh
+
 exit 0
