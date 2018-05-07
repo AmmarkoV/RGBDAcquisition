68a8356d27f27495f43f79fe36717351c69b4f94
Ammar Qammaz <ammarkov@gmail.com>
2018-05-08 02:35:02 +0300

    ...

--- testProcessorDarknet.sh
+++ testProcessorDarknet.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
  
  
-#./run_viewer.sh -module TEMPLATE -from naobasic  -noDepth  -processor ../processors/DarknetProcessor/libDarknetProcessor.so  DarknetProcessor  /home/ammar/Documents/Programming/FORTH/input_acquisition/3dparty/darknet/yolo.weights /home/ammar/Documents/Programming/FORTH/input_acquisition/3dparty/darknet/cfg/yolo.cfg  /home/ammar/Documents/Programming/FORTH/input_acquisition/3dparty/darknet/cfg/coco.data $@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR" 
+
+cd redist
+ln -s ../3dparty/darknet/data
+cd ..
+
+cd .. 
+ln -s $DIR/3dparty
+cd $DIR
+
+
+#./run_viewer.sh -module TEMPLATE -from naobasic  -noDepth  -processor ../processors/DarknetProcessor/libDarknetProcessor.so  DarknetProcessor  $DIR/3dparty/darknet/yolo.weights $DIR/3dparty/darknet/cfg/yolo.cfg  $DIR/3dparty/darknet/cfg/coco.data $@
  
-./run_viewer.sh -module V4L2 -from /dev/video0  -noDepth -processor ../processors/DarknetProcessor/libDarknetProcessor.so  DarknetProcessor  /home/ammar/Documents/Programming/FORTH/input_acquisition/3dparty/darknet/yolo.weights /home/ammar/Documents/Programming/FORTH/input_acquisition/3dparty/darknet/cfg/yolo.cfg  /home/ammar/Documents/Programming/FORTH/input_acquisition/3dparty/darknet/cfg/coco.data --payload ./payload.sh $@
+./run_viewer.sh -module V4L2 -from /dev/video0  -noDepth -processor ../processors/DarknetProcessor/libDarknetProcessor.so  DarknetProcessor  $DIR/3dparty/darknet/yolo.weights $DIR/3dparty/darknet/cfg/yolo.cfg  $DIR/3dparty/darknet/cfg/coco.data --payload ./payload.sh $@
  
-#./run_viewer.sh -module DESKTOP -from /dev/video0  -noDepth -noColor  -processor ../processors/DarknetProcessor/libDarknetProcessor.so  DarknetProcessor  /home/ammar/Documents/Programming/FORTH/input_acquisition/3dparty/darknet/yolo.weights /home/ammar/Documents/Programming/FORTH/input_acquisition/3dparty/darknet/cfg/yolo.cfg  /home/ammar/Documents/Programming/FORTH/input_acquisition/3dparty/darknet/cfg/coco.data $@
-exit 0
+#./run_viewer.sh -module DESKTOP -from /dev/video0  -noDepth -noColor  -processor ../processors/DarknetProcessor/libDarknetProcessor.so  DarknetProcessor  $DIR/3dparty/darknet/yolo.weights $DIR/3dparty/darknet/cfg/yolo.cfg  $DIR/3dparty/darknet/cfg/coco.data $@
+exit 0 
