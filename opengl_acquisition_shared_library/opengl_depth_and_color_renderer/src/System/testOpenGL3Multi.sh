59e34f4b659297d248bc6b7a2e47f05b4ffae7a1
Ammar Qammaz <ammarkov@ics.forth.gr>
2018-06-22 14:10:41 +0300

    ...

---
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/System/testOpenGL3Multi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm gl3Multi
+gcc -o gl3Tex glx3.c glx_testMultiViewport.c ../Rendering/ShaderPipeline/shader_loader.c ../Rendering/downloadFromRenderer.c ../Tools/save_to_file.c ../../../../tools/AmMatrix/matrix4x4Tools.c ../../../../tools/AmMatrix/matrixOpenGL.c -lm -lGL -lGLU -lX11 -lGLEW
+./gl3Multi
+
+exit 0
--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/System/testOpenGL3RenderToTexture.sh
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/System/testOpenGL3RenderToTexture.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm gl3
+rm gl3Tex
 gcc -o gl3Tex glx3.c glx_testRenderToTexture.c ../Rendering/ShaderPipeline/shader_loader.c ../Rendering/downloadFromRenderer.c ../Tools/save_to_file.c ../../../../tools/AmMatrix/matrix4x4Tools.c ../../../../tools/AmMatrix/matrixOpenGL.c -lm -lGL -lGLU -lX11 -lGLEW
 ./gl3Tex
 
