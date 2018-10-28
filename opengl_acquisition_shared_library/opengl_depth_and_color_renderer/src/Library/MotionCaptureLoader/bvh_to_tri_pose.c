ca2dba3596663b2a1c7f71696356ec9fdaea79a1
Ammar Qammaz <ammarkov@gmail.com>
2018-10-28 12:12:48 +0200

    ...

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/bvh_to_tri_pose.c
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/bvh_to_tri_pose.c
@@ -1,1 +1,8 @@
 #include "bvh_to_tri_pose.h"
+
+
+int bvh_loadBVHToTRI(const char * filename , struct bvhToTRI * bvhtri)
+{
+
+return 0;
+}
--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/bvh_to_tri_pose.h
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/bvh_to_tri_pose.h
@@ -1,6 +1,17 @@
 #ifndef BVH_TO_TRI_POSE_H_INCLUDED
 #define BVH_TO_TRI_POSE_H_INCLUDED
 
+#include "bvh_loader.h"
 
+struct BVH_JointAssocation
+{
+  const char * bvhJointName[MAX_BVH_JOINT_NAME];
+  const char * triJointName[MAX_BVH_JOINT_NAME];
+};
+
+struct bvhToTRI
+{
+ struct BVH_JointAssocation * jointAssociation[MAX_BVH_JOINT_HIERARCHY_SIZE];
+};
 
 #endif // BVH_TO_TRI_POSE_H_INCLUDED
