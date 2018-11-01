5ab6d621eaa9dcd759641737dc38dcfbd0656abe
Ammar Qammaz <ammarkov@ics.forth.gr>
2018-11-01 18:57:09 +0200

    refreshed all my TRI files..

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/submodules/Assimp/refreshTRIModels.sh
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/submodules/Assimp/refreshTRIModels.sh
@@ -4,9 +4,26 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 
 
-./assimpTester --convert ../../ScannedModels/Ammar/AmmarRigged.dae Ammar.tri
-./assimpTester --convert ../../ScannedModels/Elina/ElinaRigged.dae Elina.tri
-./assimpTester --convert ../../ScannedModels/Dennis2/DennisRigged2.dae Dennis2.tri
-./assimpTester --convert ../../ScannedModels/Aggeliki2/Aggeliki2Rigged.dae Aggeliki2.tri
+./assimpTester --convert ../../ScannedModels/Ammar/AmmarRigged.dae AmmarO.tri
+./assimpTester --convert ../../ScannedModels/Elina/ElinaRigged.dae ElinaO.tri
+./assimpTester --convert ../../ScannedModels/Dennis2/DennisRigged2.dae Dennis2O.tri
+./assimpTester --convert ../../ScannedModels/Aggeliki2/Aggeliki2Rigged.dae Aggeliki2O.tri
+
+
+./assimpTester --convert AmmarRiggedH.dae AmmarH.tri
+./assimpTester --convert AmmarRiggedM.dae AmmarM.tri
+./assimpTester --convert AmmarRiggedS.dae AmmarS.tri
+
+
+./assimpTester --convert Ammar_1k.dae Ammar_1k.tri
+./assimpTester --convert Ammar_7k.dae Ammar_7k.tri
+./assimpTester --convert Ammar_18k.dae Ammar_18k.tri 
+
+./assimpTester --convert ElinaRiggedH.dae ElinaH.tri
+./assimpTester --convert ElinaRiggedS.dae ElinaS.tri
+
+
+ln -s AmmarH.tri Ammar.tri
+ln -s ElinaH.tri Elina.tri
 
 exit 0
