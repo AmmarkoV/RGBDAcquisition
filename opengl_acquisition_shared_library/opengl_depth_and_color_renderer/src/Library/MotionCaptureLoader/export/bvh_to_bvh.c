d603c5b874f58ebedfd173847ee9d6da6813e007
Ammar Qammaz <ammarkov@gmail.com>
2018-11-17 14:45:11 +0200

    adding bvh to bvh exporter..

---
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/export/bvh_to_bvh.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bvh_to_bvh.h"
+
+#include "../bvh_project.h"
+
+int dumpBVHToBVH(
+                  const char * svgFilename,
+                  struct BVH_MotionCapture * mc,
+                  struct BVH_Transform * bvhTransform,
+                  unsigned int fID,
+                  struct simpleRenderer * renderer,
+                  float * objectRotationOffset
+                 )
+{
+ return 0;
+}
+
