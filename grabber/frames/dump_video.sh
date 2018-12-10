2f5d2f2741d56cfab2ac756e59562e0e6cd12e45
Ammar Qammaz <ammarkov@ics.forth.gr>
2018-12-10 15:43:49 +0200

    added new script to dump video files to frames..

---
+++ grabber/frames/dump_video.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+
+STARTDIR=`pwd`
+#Switch to this directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+DATASET=""
+
+if (( $#<1 ))
+then 
+ echo "Please provide arguments first argument is dataset "
+ exit 1
+else
+ DATASET=$1-data
+fi
+ 
+THEDATETAG=`date +"%y-%m-%d_%H-%M-%S"` 
+
+mkdir $DATASET
+
+ffmpeg -i $1  -r 30 -q:v 1  $DATASET/colorFrame_0_%05d.jpg
+
+cd $DATASET
+cd ..
+
+cd $STARTDIR 
+exit 0
+
