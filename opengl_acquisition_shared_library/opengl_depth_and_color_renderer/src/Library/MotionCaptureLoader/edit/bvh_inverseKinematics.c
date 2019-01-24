da188dd78b3e4d9b16f202c38444511584204441
Ammar Qammaz <ammarkov@ics.forth.gr>
2019-01-24 15:30:18 +0200

    adding an inverse kinematics solver for my bvh files..

---
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/edit/bvh_inverseKinematics.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bvh_inverseKinematics.h"
+ 
---
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/edit/bvh_inverseKinematics.h
@@ -0,0 +1,7 @@
+#ifndef BVH_INVERSEKINEMATICS_H_INCLUDED
+#define BVH_INVERSEKINEMATICS_H_INCLUDED
+
+
+#include "../bvh_loader.h"
+ 
+#endif
