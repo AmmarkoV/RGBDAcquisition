96c04ecb99332625f2e82eb13eb2232993e80d19
Ammar Qammaz <ammarkov@gmail.com>
2019-06-22 17:54:47 +0300

    trying to solve compilation problems on a new pc..

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/System/testOpenGL3.sh
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/System/testOpenGL3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm gl3
-gcc -o gl3 glx3.c glx_test.c ../Rendering/ShaderPipeline/shader_loader.c ../../../../../tools/AmMatrix/matrix4x4Tools.c ../../../../../tools/AmMatrix/matrixOpenGL.c -lm -lGL -lX11 -lGLEW
+gcc -o gl3  -DUSE_GLEW glx_test.c glx3.c ../Rendering/ShaderPipeline/shader_loader.c ../../../../../tools/AmMatrix/matrix4x4Tools.c ../../../../../tools/AmMatrix/matrixOpenGL.c -lm -lGLEW -lGL -lX11 
 ./gl3
 
 exit 0
