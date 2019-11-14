76ce358c128e4fe738d4ecd052531902c8770ec5
Ammar Qammaz <ammarkov@gmail.com>
2019-11-15 00:39:04 +0200

    adding some bvh samples..

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/edit/bvh_merge.c
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/edit/bvh_merge.c
@@ -1,7 +1,7 @@
 #include "bvh_merge.h"
+#include "../bvh_to_tri_pose.h"
 
-
-// ./BVHTester --from Motions/02_03.bvh --merge Motions/49_04.bvh Motions/mergeDazFriendlyMoreThan19.profile --to testMerge.bvh
+// ./BVHTester --from Motions/02_03.bvh --merge Motions/49_04.bvh Motions/mergeDazFriendlyMoreThan19.profile --bvh testMerge.bvh
 
 int bvh_mergeWith(
                    struct BVH_MotionCapture * mc,
@@ -9,7 +9,8 @@ int bvh_mergeWith(
                    const char * pathToMergeRules
                  )
 {
+ struct bvhToTRI bvhtri={0};
+ bvh_loadBVHToTRIAssociationFile(pathToMergeRules,&bvhtri,mc);
 
-
-  return 0;
+ return 0;
 }
