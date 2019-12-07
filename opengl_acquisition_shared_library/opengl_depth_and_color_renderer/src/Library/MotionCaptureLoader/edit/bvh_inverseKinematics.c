5477451e7dc4e4612262ad152913b5a7e0911d90
Ammar Qammaz <ammarkov@gmail.com>
2019-12-07 20:38:27 +0200

    ...

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/edit/bvh_inverseKinematics.c
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/edit/bvh_inverseKinematics.c
@@ -6,11 +6,18 @@
 
 
 
+float BVH2DDistace(struct simpleRenderer *renderer,struct BVH_Transform * bvhSourceTransform,struct BVH_Transform * bvhTargetTransform)
+{
+
+
+ return 0.0;
+}
+
+
 
 
 int BVHTestIK(
               struct BVH_MotionCapture * mc,
-              struct simpleRenderer * renderer,
               unsigned int fIDSource,
               unsigned int fIDTarget
              )
@@ -18,13 +25,22 @@ int BVHTestIK(
   struct BVH_Transform bvhSourceTransform={0};
   struct BVH_Transform bvhTargetTransform={0};
 
+  struct simpleRenderer renderer={0};
+  simpleRendererDefaults(
+                         &renderer,
+                         1920, 1080, 582.18394,   582.52915 // https://gopro.com/help/articles/Question_Answer/HERO4-Field-of-View-FOV-Information
+                        );
+  simpleRendererInitialize(&renderer);
+
   if (
        ( bvh_loadTransformForFrame(mc,fIDSource,&bvhSourceTransform) )
         &&
        ( bvh_loadTransformForFrame(mc,fIDTarget,&bvhTargetTransform) )
      )
      {
+        float distance2D = BVH2DDistace(&renderer,&bvhSourceTransform,&bvhTargetTransform);
 
+        fprintf(stderr,"2D Distance is %0.2f\n",distance2D);
         return 1;
      }
 
