e6cfd138d32a0f97ee4dbe6f0ea207582a059ea3
Ammar Qammaz <ammarkov@gmail.com>
2020-02-12 00:42:39 +0200

    ...

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/export/bvh_export.c
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/export/bvh_export.c
@@ -4,6 +4,17 @@
 #include "bvh_to_csv.h"
 #include "bvh_to_svg.h"
 
+
+#define NORMAL   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+
 int performPointProjectionsForFrame(
                                      struct BVH_MotionCapture * mc,
                                      struct BVH_Transform * bvhTransform,
@@ -151,7 +162,7 @@ int dumpBVHToSVGCSV(
                                        )
        )
    {
-       fprintf(stderr,"Could not perform projection for frame %u\n",fID);
+       fprintf(stderr,RED "Could not perform projection for frame %u\n" NORMAL,fID);
    }
 
 
