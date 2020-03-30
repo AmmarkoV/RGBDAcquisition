7f1302d4f7b136ee9a7e9819d2d9ab5eca02316b
Ammar Qammaz <ammarkov@gmail.com>
2020-03-30 03:55:58 +0300

    adding a script to profile ik code, 40% of the time is multiply4x4 matrices, however I can take advantage of only changing specific joints and avoid a lot of operations..

---
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/ik/profile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
+cd ..
+cd ..
+cd ..
+cd ..
+
+
+rm cachegrind*
+
+valgrind --tool=cachegrind ./BVHTester --from Motions/05_01.bvh --selectJoints 0 23 hip eye.r eye.l abdomen chest neck head rshoulder relbow rhand lshoulder lelbow lhand rhip rknee rfoot lhip lknee lfoot toe1-2.r toe5-3.r toe1-2.l toe5-3.l --testIK 4 100
+
+FILE=`ls | grep cach*`
+
+kcachegrind $FILE
+
+exit 0
