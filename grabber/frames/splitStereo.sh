8460edee42c90a10ffd9787b869d21a83b6cd886
Ammar Qammaz <ammarkov@gmail.com>
2020-09-16 15:41:24 +0300

    new stereo dataset holder..

--- grabber/frames/splitStereo.sh
+++ grabber/frames/splitStereo.sh
@@ -17,19 +17,28 @@ fi
 FULL_WIDTH=`ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 $INPUTFILE`
 WIDTH=`expr $FULL_WIDTH / 2`
 HEIGHT=`ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 $INPUTFILE`
+FRAMERATER=`ffprobe -v error -select_streams v:0 -show_entries stream=avg_frame_rate -of csv=s=x:p=0 $INPUTFILE`
+FRAMERATE=`expr $FRAMERATER`
 
 echo "File size is $FULL_WIDTH x $HEIGHT"
 echo "Each feed is $WIDTH"
 #exit 0
 
-mkdir $DATASET
+mkdir -p $DATASET/calib
 
-ffmpeg -i $1  -r 30 -q:v 1 -filter:v "crop=$WIDTH:$HEIGHT:0:0"  $DATASET/colorFrame_0_%05d.jpg
- 
-cp $DATASET/colorFrame_0_00001.jpg $DATASET/colorFrame_0_00000.jpg
+echo "Left feed is 0_color and Right 1_color" > $DATASET/README
+
+#---------------------------------------------------------------------
+mkdir -p $DATASET/0_color
+ffmpeg -i $1  -r $FRAMERATE -q:v 1 -filter:v "crop=$WIDTH:$HEIGHT:0:0"  $DATASET/0_color/%05d.jpg
+cp $DATASET/0_color/00001.jpg $DATASET/0_color/00000.jpg
 
 
-ffmpeg -i $1  -r 30 -q:v 1 -filter:v "crop=$WIDTH:$HEIGHT:$WIDTH:0"  $DATASET/colorFrame_1_%05d.jpg
+#---------------------------------------------------------------------
+mkdir -p $DATASET/1_color
+ffmpeg -i $1  -r $FRAMERATE -q:v 1 -filter:v "crop=$WIDTH:$HEIGHT:$WIDTH:0"  $DATASET/1_color/%05d.jpg
+cp $DATASET/1_color/00001.jpg $DATASET/1_color/00000.jpg
 
-cp $DATASET/colorFrame_1_00001.jpg $DATASET/colorFrame_1_00000.jpg
+
+#---------------------------------------------------------------------
 exit 0
