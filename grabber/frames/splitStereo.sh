9b834c8cb78d8ee5f92eb09c7ebd07de1ec47035
Ammar Qammaz <ammarkov@gmail.com>
2020-09-16 15:15:51 +0300

    new split stereo feed..

--- grabber/frames/splitStereo.sh
+++ grabber/frames/splitStereo.sh
@@ -6,16 +6,30 @@ INPUTFILE="pnm"
 
 if (( $#<1 ))
 then 
- echo "Please provide arguments first argument is dataset ,  second is file format ( i.e. boxNew jpg ) "
+ echo "Please provide arguments first argument is dataset ,  second is file format ( i.e. boxNew.mp4 ) "
  exit 1
 else
  INPUTFILE=$1 
+ DATASET=$1-data
 fi
  
 
-convert $INPUTFILE -crop 720x416+0+0 part1.jpg
-convert $INPUTFILE -crop 720x416+771+0 part2.jpg
+FULL_WIDTH=`ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=s=x:p=0 $INPUTFILE`
+WIDTH=`expr $FULL_WIDTH / 2`
+HEIGHT=`ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 $INPUTFILE`
 
-convert -delay 10  -size 720x416 -page +0+0  part1.jpg -page +0+0  part2.jpg -loop 0  animation.gif
+echo "File size is $FULL_WIDTH x $HEIGHT"
+echo "Each feed is $WIDTH"
+#exit 0
 
+mkdir $DATASET
+
+ffmpeg -i $1  -r 30 -q:v 1 -filter:v "crop=$WIDTH:$HEIGHT:0:0"  $DATASET/colorFrame_0_%05d.jpg
+ 
+cp $DATASET/colorFrame_0_00001.jpg $DATASET/colorFrame_0_00000.jpg
+
+
+ffmpeg -i $1  -r 30 -q:v 1 -filter:v "crop=$WIDTH:$HEIGHT:$WIDTH:0"  $DATASET/colorFrame_1_%05d.jpg
+
+cp $DATASET/colorFrame_1_00001.jpg $DATASET/colorFrame_1_00000.jpg
 exit 0
