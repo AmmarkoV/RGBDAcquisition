3a8266fd4a07433606247fc190aaf004c5183a1e
Ammar Qammaz <ammarkov@gmail.com>
2020-10-03 00:48:43 +0300

    added a function to get realtime priority..

--- tools/PthreadWorkerPool/pthreadWorkerPool.h
+++ tools/PthreadWorkerPool/pthreadWorkerPool.h
@@ -65,6 +65,54 @@ static int nanoSleepT(long nanoseconds)
    return nanosleep(&req , &rem);
 }
 
+
+
+
+#include <sched.h>
+ 
+static int set_realtime_priority() 
+{
+     int ret;
+ 
+     // We'll operate on the currently running thread.
+     pthread_t this_thread = pthread_self();
+     // struct sched_param is used to store the scheduling priority
+     struct sched_param params;
+ 
+     // We'll set the priority to the maximum.
+     params.sched_priority = sched_get_priority_max(SCHED_FIFO);
+
+     fprintf(stderr,"Trying to set thread realtime prio = %u \n",params.sched_priority);
+ 
+     // Attempt to set thread real-time priority to the SCHED_FIFO policy
+     ret = pthread_setschedparam(this_thread, SCHED_FIFO, &params);
+     if (ret != 0) {
+                     // Print the error
+                     fprintf(stderr,"Failed setting thread realtime priority\n");
+                     return 0;     
+                   }
+     
+       // Now verify the change in thread priority
+     int policy = 0;
+     ret = pthread_getschedparam(this_thread, &policy, &params);
+     if (ret != 0) {
+                     fprintf(stderr,"Couldn't retrieve real-time scheduling paramers\n");
+                     return 0;
+                   }
+ 
+     // Check the correct policy was applied
+     if(policy != SCHED_FIFO) { fprintf(stderr,"Scheduling is NOT SCHED_FIFO!\n"); } else 
+                              { fprintf(stderr,"SCHED_FIFO OK\n"); }
+ 
+     // Print thread scheduling priority
+     fprintf(stderr,"Thread priority is now %u\n",params.sched_priority); 
+     return 0;
+}
+
+
+ 
+
+
 static int threadpoolWorkerInitialWait(struct threadContext * ctx)
 {  
   //fprintf(stderr,"Thread-%u: pthread_mutex_lock(&ctx->pool->startWorkMutex);\n",ctx->threadID);
