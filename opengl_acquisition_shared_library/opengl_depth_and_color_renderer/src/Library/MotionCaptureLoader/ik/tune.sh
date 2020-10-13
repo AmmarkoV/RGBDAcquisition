0e6cf8ddcf5e14f03298d431c981a371ff6769b0
Ammar Qammaz <ammarkov@gmail.com>
2020-10-13 17:05:13 +0300

    ...

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/ik/tune.sh
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/ik/tune.sh
@@ -15,7 +15,8 @@ cd ..
 #set view 45,45; set view map; set yrange [9:90]; set zrange [30:90];
 # set dgrid3d 25,25 qnorm 4;
 #splot \"tuneIterations.dat\" using 1:3:2 with points pointsize 3 pointtype 7; 
-GNUPLOT_CMD="set terminal png size 1920,1400 font \"Helvetica,40\"; set output \"tuneIterations.png\";  set isosample 160; set pm3d at b; set palette defined (65 \"black\", 70 \"red\", 80 \"yellow\", 90 \"yellow\", 100 \"white\");  set view 45,45;  set hidden3d; set xrange [1:25]; set yrange [9:90]; set zrange [30:90]; set style fill solid; set xlabel \"HCD Iterations\" rotate parallel; set ylabel \"Frames per second\" rotate parallel; set zlabel \"Mean average error in mm\" rotate parallel; set title \"HCD tuning iteration hyperparameter\"; set ytics 20; set multiplot; splot \"tuneIterations.dat\" using 1:3:2 with lines palette lw 2 title \" \"; splot \"tuneIterations.dat\" using 1:3:2 with points palette pointsize 1 pointtype 7 title \" \";  " 
+#HCD tuning iteration hyperparameter
+GNUPLOT_CMD="set terminal png size 1000,800 font \"Helvetica,34\"; set output \"tuneIterations.png\";  set isosample 160; set pm3d at b; set palette defined (65 \"black\", 70 \"red\", 80 \"yellow\", 90 \"yellow\", 100 \"white\");  set view 30,45;  set hidden3d; set xrange [1:25]; set yrange [9:90]; set zrange [30:90]; set style fill solid; set xlabel \"HCD Iterations\" rotate parallel; set ylabel \"Frames per second\" rotate parallel; set zlabel \"Mean average error in mm\" rotate parallel; set ztics 30; set title \"Mean average error in mm\"; set ytics 30; set multiplot; splot \"tuneIterations.dat\" using 1:3:2 with lines palette lw 2 title \" \"; splot \"tuneIterations.dat\" using 1:3:2 with points palette pointsize 1 pointtype 7 title \" \";  " 
 
 gnuplot -e "$GNUPLOT_CMD"
 
