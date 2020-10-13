e4fb818d836c4ede9c57cb2bf0e1a20e7857a21e
Ammar Qammaz <ammarkov@gmail.com>
2020-10-13 08:33:04 +0300

    ...

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/ik/tune.sh
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/ik/tune.sh
@@ -13,7 +13,7 @@ cd ..
 
 # plot $map1 matrix using 1:2:3 with image,   $map1 matrix using 1:2:($3 == 0 ? "" : sprintf("%g",$3) ) with labels
 
-GNUPLOT_CMD="set terminal png; set output \"tuneIterations.png\";  plot \"tuneIterations.dat\" matrix columnheaders using 1:2:3 with image"
+GNUPLOT_CMD="set terminal png; set output \"tuneIterations.png\"; set xlabel \"Iterations\"; set ylabel \"Framerate\"; set zlabel \"Accuracy\";  set ticslevel 0; set samples 25, 25; set isosamples 50, 50; splot \"tuneIterations.dat\" matrix using 1:2:3 with pm3d title \"Iteration benchmark\""
 gnuplot -e "$GNUPLOT_CMD"
 
 exit 0
