d561b091ef70ba4aa4237ec9f5824194f94a0b34
Ammar Qammaz <ammarkov@gmail.com>
2021-10-29 11:53:41 +0300

    less verbose apache2 php server..

--- 3dparty/ROS/camera_broadcast/apache/upload.php
+++ 3dparty/ROS/camera_broadcast/apache/upload.php
@@ -4,40 +4,46 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
+//Debug incoming request headers
 //print_r($_SERVER)
-$headers =  getallheaders();
-foreach($headers as $key=>$val){
-  echo $key . ': ' . $val . '<br>';
-}
-
+//$headers =  getallheaders();
+//foreach($headers as $key=>$val)
+//{
+//  echo $key . ': ' . $val . '<br>';
+//}
 
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
+if(isset($_POST["submit"])) 
+  {
+   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+   if($check !== false) 
+     {
+      echo "File is an image - " . $check["mime"] . ".";
+      $uploadOk = 1;
+     } else 
+     {
+      echo "File is not an image.";
+      $uploadOk = 0;
+     }
+   }
 
 // Check if file already exists
-if (file_exists($target_file)) {
-  echo "Override file already exists.";
+if (file_exists($target_file)) 
+{
+  echo "Overwriting ";
   $uploadOk = 1;
 }
 
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 8000000) {
+if ($_FILES["fileToUpload"]["size"] > 8000000) 
+{
   echo "Sorry, your file is too large.";
   $uploadOk = 0;
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) 
+{
   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed ( not ".$imageFileType .").";
   echo "Target File".$targetFile." !";
   //$targetFile = "error.jpg"
@@ -45,14 +51,19 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
 }
 
 // Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
+if ($uploadOk == 0) 
+{
   echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
+ // if everything is ok, try to upload file
+} else 
+{
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) 
+    {
+     echo " ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " uploaded.";
+    } else 
+    {
+     echo "Sorry, there was an error uploading your file.";
+    }
 }
 ?>
+
