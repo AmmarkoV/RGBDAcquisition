11a4ce0f72a015784d82a3821673610503792677
Ammar Qammaz <ammarkov@gmail.com>
2022-03-01 02:38:11 +0200

    more stuff on the python BVH wrapper..

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Applications/BVHTester/bvhLibrary.py
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Applications/BVHTester/bvhLibrary.py
@@ -4,6 +4,31 @@ import sys
 from ctypes import *
 from os.path import exists
 
+#--------------------------------------------------------
+def gatherAllBVHFiles(directoryPath):
+   results = list()
+   for f in os.scandir(directoryPath):
+       if f.is_file():
+          #print("f.path=",f.path)
+          if (f.path.find(".bvh")!=-1):
+              print("Adding BVH file ",f.path)
+              results.append(f.path)
+   return results;
+#--------------------------------------------------------
+def gatherAllBVHDirectories(directoryPath):
+   results = list()
+   for f in os.scandir(directoryPath):
+       if f.is_dir():
+          print("Adding files in ",f.path)
+          results = results + gatherAllBVHFiles(f.path)
+   return results
+#--------------------------------------------------------
+def writeListToFile(theList,theFilename):
+   file = open(theFilename, "w")
+   for theList in allBVHFiles:
+      file.write(theList + "\n")
+   file.close()
+#--------------------------------------------------------
 
 def loadLibrary(filename):
  if not exists(filename):
@@ -12,15 +37,13 @@ def loadLibrary(filename):
  if not exists(filename):
      print("Could not make BVH Library, terminating")
      sys.exit(0)
-
  libBVH = CDLL(filename)
  #call C function to check connection
  libBVH.connect() 
  libBVH.bvhConverter.restype = c_int
  libBVH.bvhConverter.argtypes = c_int,POINTER(c_char_p)
-
  return libBVH
-
+#--------------------------------------------------------
 def bvhConvert(libBVH,arguments):
     argumentBytes = []
     for i in range(len(arguments)):
@@ -29,24 +52,128 @@ def bvhConvert(libBVH,arguments):
     argv[:] = argumentBytes 
     argc=len(argumentBytes)
     libBVH.bvhConverter(argc,argv)
+#--------------------------------------------------------
 
 
 libBVH = loadLibrary("./libBVHConverter.so")
 
-startingFlags=list()
-
-
+pythonFlags=list()
 #Add any arguments given in the python script directly!
 if (len(sys.argv)>1):
    print('Supplied argument List:', str(sys.argv))
    for i in range(0, len(sys.argv)):
-     startingFlags.append(sys.argv[i])
+     pythonFlags.append(sys.argv[i])
+#------------------------------------------------------    
 
-   
-startingFlags.append("--test")
-startingFlags.append("--test 123")
-startingFlags.append("--printparams")
  
-bvhConvert(libBVH,startingFlags)
+
+outputDirectory = os.getcwd()
+allBVHFiles = gatherAllBVHDirectories("/home/ammar/Documents/Programming/DNNTracker/DNNTracker/dataset/MotionCapture") 
+print("Output Directory = ",outputDirectory)
+print("Found ",len(allBVHFiles)," BVH files")
+
+#Keep list for debug.. 
+writeListToFile(allBVHFiles,"listOfBVHFiles.txt")
+
+
+bodyPart="upperbody"
+os.system("rm 2d_"+bodyPart+".csv")
+os.system("rm bvh_"+bodyPart+".csv")
+
+
+minRotationLimit="-45"
+maxRotationLimit="45"
+minORIENTATION="-55" #-45  default , -55 with 10 deg safety  
+maxORIENTATION="55"  # 45  default , 55 with 10 deg safety
+minDepth="900"  #1000 original
+maxDepth="4500" #3000 is too small
+
+#HIDEBODY="--hide2DLocationOfJoints 0 8 abdomen chest eye.r eye.l toe1-2.r toe5-3.r toe1-2.l toe5-3.l" #We want to contain these joints in the BVH file and solve them, but they do not
+#SELECTBODY="--selectJoints 1 23 hip eye.r eye.l abdomen chest neck head rshoulder relbow rhand lshoulder lelbow lhand rhip rknee rfoot lhip lknee lfoot toe1-2.r toe5-3.r toe1-2.l toe5-3.l $HIDEBODY"
+#--randomize2D $MIN_DEPTH $MAX_DEPTH $MIN_LIM $FRONT_MIN_ORIENTATION $MIN_LIM $MAX_LIM $FRONT_MAX_ORIENTATION $MAX_LIM" "$SELECTLOWERBODY $RAND_LOWER_BODY
+
+#$MIN_DEPTH $MAX_DEPTH $MIN_LIM -179.999999 $MIN_LIM $MAX_LIM 180 $MAX_LIM
+
+
+for bvhFile in allBVHFiles:
+    args=list()
+    args.append("--printparams") 
+    args.append("--haltonerror")
+    args.append("--from")
+    args.append(bvhFile)
+#RAND_UPPER_BODY="--perturbJointAngles 2 30.0 rshoulder lshoulder --perturbJointAngles 2 16.0 relbow lelbow --perturbJointAngles 2 10.0 abdomen chest"
+#RAND_LOWER_BODY="--perturbJointAngles 2 30.0 rhip lhip --perturbJointAngles 4 10.0 lknee rknee lfoot rfoot --perturbJointAngles 2 10.0 abdomen chest"
+    args.append("--filtergimballocks"); args.append("4")
+    #startingFlags.append("--repeat")          ;  startingFlags.append("1")
+
+    #Hip Position/Rotation Randomization
+    args.append("--randomize2D")
+    args.append(minDepth)  #Minimum distance from the camera
+    args.append(maxDepth)  #Maximum distance from the camera
+    args.append(minRotationLimit)  #
+    args.append("-179.99")
+    args.append(minRotationLimit)
+    args.append(maxRotationLimit)
+    args.append("179.99")
+    args.append(maxRotationLimit)
+    #-------------------------------------
  
+
+    #Upper body joints
+    #-------------------------------------
+    args.append("--selectJoints") 
+    args.append("1")
+    args.append("23")
+    args.append("hip")
+    args.append("eye.r")
+    args.append("eye.l")
+    args.append("abdomen")
+    args.append("chest")
+    args.append("neck")
+    args.append("head")
+    args.append("rshoulder")
+    args.append("relbow")
+    args.append("rhand")
+    args.append("lshoulder")
+    args.append("lelbow")
+    args.append("lhand")
+    args.append("rhip")
+    args.append("rknee")
+    args.append("rfoot")
+    args.append("lhip")
+    args.append("lknee")
+    args.append("lfoot")
+    args.append("toe1-2.r")
+    args.append("toe5-3.r")
+    args.append("toe1-2.l")
+    args.append("toe5-3.l")
+    #-------------------------------------
+
+    #Deactivate spare joints
+    #-------------------------------------
+    args.append("--hide2DLocationOfJoints")
+    args.append("0")
+    args.append("8")
+    args.append("abdomen")
+    args.append("chest")
+    args.append("eye.r")
+    args.append("eye.l")
+    args.append("toe1-2.r")
+    args.append("toe5-3.r")
+    args.append("toe1-2.l")
+    args.append("toe5-3.l")
+    #-------------------------------------
+
+
+    args.append("--occlusions") 
+    args.append("--csv") 
+    args.append(outputDirectory) 
+    args.append(bodyPart+".csv") 
+    args.append("2d+bvh")
+    args = args + pythonFlags
+    bvhConvert(libBVH,args)
+
+#./GroundTruthDumper $VIEW_COMMANDS --haltonerror --from $BVHFILE --filtergimballocks 4 $3 --repeat $ITERATIONS $2 --occlusions --csv $outputDir $1 2d+bvh # --bvh $outputDir/$f-random.bvh
+
+
 
