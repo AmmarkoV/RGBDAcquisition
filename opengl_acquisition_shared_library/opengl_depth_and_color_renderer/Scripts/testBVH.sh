ce71a025c255922051fd2aaae3d5131e688c7675
Ammar Qammaz <ammarkov@gmail.com>
2022-04-10 11:29:50 +0300

    ...

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/Scripts/testBVH.sh
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/Scripts/testBVH.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$THISDIR"
+cd ..
 #./BVHTester --from $1 --offsetPositionRotation 0 750 2000 0 0 0 --occlusions  --svg svg # --bvh $outputDir/$f-random.bvh
 
 
