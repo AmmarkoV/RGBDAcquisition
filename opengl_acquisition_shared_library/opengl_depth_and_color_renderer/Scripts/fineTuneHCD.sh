e106ac6ced9b500626c246a8fed6efd958a422f5
Ammar Qammaz <ammarkov@gmail.com>
2022-12-13 02:59:45 +0200

    ...

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/Scripts/fineTuneHCD.sh
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/Scripts/fineTuneHCD.sh
@@ -21,13 +21,13 @@ EPOCHS="10"
 LANGEVIN_DYNAMICS="0.3"
 VERBOSITY="0"
 
-for EPOCHS in `echo "5 10 15"`
+for EPOCHS in `echo "15"`
 do
- for ITERATIONS in `echo "5 10 15"`
+ for ITERATIONS in `echo "10 11 12 13 14 15"`
  do
-  for LR  in `echo "0.01 0.03 0.06 0.09 0.1"`
+  for LR  in `echo "0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.01"`
   do
-   for LANGEVIN_DYNAMICS  in `echo "0.1 0.2 0.3 0.4 0.5"`
+   for LANGEVIN_DYNAMICS  in `echo "0.0 0.05 0.1 0.15 0.2 0.25"`
    do
       echo "IT: $ITERATIONS | EP: $EPOCHS | LR: $LR | LANGV: $LANGEVIN_DYNAMICS"
       ./BVHTester --from Motions/05_01.bvh --selectJoints 0 23 hip eye.r eye.l abdomen chest neck head rshoulder relbow rhand lshoulder lelbow lhand rhip rknee rfoot lhip lknee lfoot toe1-2.r toe5-3.r toe1-2.l toe5-3.l --testIK $PREVIOUS_FRAME $CURRENT_FRAME $TARGET_FRAME $STEP_FRAME $LR $ITERATIONS $EPOCHS 1 $LANGEVIN_DYNAMICS $VERBOSITY $@
