8aa8b2cb6d73315bc2dfcba3ff1a1d790942a913
Ammar Qammaz <ammarkov@ics.forth.gr>
2023-02-20 15:23:17 +0200

    doing an HCD fine tuning experiment..

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/Scripts/fineTuneHCD.sh
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/Scripts/fineTuneHCD.sh
@@ -27,33 +27,35 @@ STEP_FRAME="20"
 LR="0.0020"
 ITERATIONS="20"
 EPOCHS="10"
-LANGEVIN_DYNAMICS="0.002"
+LANGEVIN_DYNAMICS="0.0"
 VERBOSITY="0"
 LEARNING_RATE_DECAY="0.8"
 MOMENTUM="0.42"
 
-for LEARNING_RATE_DECAY in ` seq 0.1 0.1 0.9 | tr "," "." `
-do
- for MOMENTUM in ` seq 0.12 0.1 0.92 | tr "," "." `
- do
-  for EPOCHS in `echo "15"`
+#for LEARNING_RATE_DECAY in ` seq 0.1 0.1 0.9 | tr "," "." `
+#do
+ #for MOMENTUM in ` seq 0.12 0.1 0.92 | tr "," "." `
+ #do
+  for EPOCHS in ` seq 5 1 10 | tr "," "." `
   do
-   for ITERATIONS in ` seq 25 1 25 | tr "," "." `
+   for ITERATIONS in ` seq 5 1 15 | tr "," "." `
    do
-    for LR  in ` seq 0.0018 0.0001 0.0076 | tr "," "." `
+    for LR  in ` seq 0.018 0.001 0.076 | tr "," "." `
     do
-     for LANGEVIN_DYNAMICS  in `seq 0.0 0.001 0.0 | tr "," "."`
-     do
+     #for LANGEVIN_DYNAMICS  in `seq 0.0 0.001 0.0 | tr "," "."`
+     #do
       echo "IT: $ITERATIONS | EP: $EPOCHS | LR: $LR | LANGV: $LANGEVIN_DYNAMICS"
       ./BVHTester --from $DATASET --addfrom $DATASETA --addfrom $DATASETB --addfrom $DATASETC --selectJoints 0 23 hip eye.r eye.l abdomen chest neck head rshoulder relbow rhand lshoulder lelbow lhand rhip rknee rfoot lhip lknee lfoot toe1-2.r toe5-3.r toe1-2.l toe5-3.l --testIK $PREVIOUS_FRAME $CURRENT_FRAME $TARGET_FRAME $STEP_FRAME $LR $ITERATIONS $EPOCHS 1 $LANGEVIN_DYNAMICS $VERBOSITY $LEARNING_RATE_DECAY $MOMENTUM $@
-     done
+     #done
     done
    done 
   done
- done
-done
+ #done
+#done
 
-python3 Scripts/plotFineTune.py learningRate lrdecay momentum mae fps 
+python3 Scripts/plotFineTune.py learningRate iterations epochs mae fps 
+
+#python3 Scripts/plotFineTune.py learningRate lrdecay momentum mae fps 
 #python3 Scripts/plotFineTune.py learningRate iterations langevin mae fps   
 
 exit 0
