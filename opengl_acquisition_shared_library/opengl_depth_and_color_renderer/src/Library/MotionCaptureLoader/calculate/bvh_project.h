cc55eabb1acd974ce12298f7325e27cd9569c01a
Ammar Qammaz <ammarkov@gmail.com>
2023-06-17 03:35:41 +0300

    ...

--- opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/calculate/bvh_project.h
+++ opengl_acquisition_shared_library/opengl_depth_and_color_renderer/src/Library/MotionCaptureLoader/calculate/bvh_project.h
@@ -10,7 +10,11 @@ extern "C"
 {
 #endif
 
-
+/**
+ * @brief Configuration parameters for the BVH renderer.
+ *
+ * The BVH_RendererConfiguration structure holds the configuration parameters for the BVH renderer. It defines the rendering settings such as width, height, frustum limits, intrinsics, distortion, projection matrix, view matrix, and viewport.
+ */
 struct BVH_RendererConfiguration
 {
   int isDefined;
@@ -32,14 +36,33 @@ struct BVH_RendererConfiguration
   int viewport[4];
 };
 
-
-
+/**
+ * @brief Cleans and initializes the BVH transform structure.
+ *
+ * This function cleans and initializes the BVH transform structure by allocating memory and setting initial values. It prepares the structure for further transformation calculations.
+ *
+ * @param mc             Pointer to the BVH_MotionCapture structure.
+ * @param bvhTransform   Pointer to the BVH_Transform structure to be cleaned and initialized.
+ */
 void bvh_cleanTransform(
                        struct BVH_MotionCapture * mc,
                        struct BVH_Transform     * bvhTransform
                       );
 
-
+/**
+ * @brief Projects a specific BVH joint onto the 2D screen space.
+ *
+ * This function projects a specified BVH joint from 3D world coordinates to 2D screen space. It uses the given BVH motion capture data, transformation data, and renderer configuration to perform the projection.
+ *
+ * @param mc              The BVH motion capture data.
+ * @param bvhTransform    The BVH transformation data.
+ * @param renderer        The renderer used for projection.
+ * @param jID             The BVH joint ID to project.
+ * @param occlusions      The number of occlusions.
+ * @param directRendering Flag indicating if direct rendering should be used.
+ *
+ * @return 1 if the joint was successfully projected, 0 otherwise.
+ */
 int bvh_projectJIDTo2D(
                      struct BVH_MotionCapture * mc,
                      struct BVH_Transform     * bvhTransform,
@@ -49,6 +72,19 @@ int bvh_projectJIDTo2D(
                      unsigned int               directRendering
                    );
 
+/**
+ * @brief Projects the BVH motion capture data onto the 2D screen space.
+ *
+ * This function projects the BVH motion capture data onto the 2D screen space using the provided transformation and renderer. It handles occlusions if specified and updates the transformation data accordingly.
+ *
+ * @param mc              The BVH motion capture data.
+ * @param bvhTransform    The BVH transformation data.
+ * @param renderer        The renderer used for projection.
+ * @param occlusions      The number of occlusions.
+ * @param directRendering Flag indicating if direct rendering should be used.
+ *
+ * @return 1 if the projection was successful, 0 otherwise.
+ */
 int bvh_projectTo2D(
                      struct BVH_MotionCapture * mc,
                      struct BVH_Transform     * bvhTransform,
@@ -61,7 +97,5 @@ int bvh_projectTo2D(
 #ifdef __cplusplus
 }
 #endif
-
-
 
 #endif // BVH_PROJECT_H_INCLUDED
